56f3bb82e6e806734b4dbb278c88cae088f1adff
Alberto Fernández <alberto.fernandezsanchez@servexternos.isban.es>
2013-09-03 15:01:03 +0200

    deleting diagram call and diagram ivr updates in diagram core, extension point for patterns

--- plugins/diagram/com.isb.jvoice.diagram.core/src/com/isb/jvoice/diagram/core/Activator.java
+++ plugins/diagram/com.isb.jvoice.diagram.core/src/com/isb/jvoice/diagram/core/Activator.java
@@ -1,7 +1,10 @@
 package com.isb.jvoice.diagram.core;
 
+import org.eclipse.equinox.log.ExtendedLogService;
+import org.eclipse.equinox.log.Logger;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
+import org.osgi.util.tracker.ServiceTracker;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -10,9 +13,13 @@ public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "com.isb.jvoice.diagram.core"; //$NON-NLS-1$
+	public static final String EQUINOX_LOGGER_NAME = "org.eclipse.equinox.logger"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
+	private static Logger logger;
+	
+
 	
 	/**
 	 * The constructor
@@ -27,6 +34,24 @@ public class Activator extends AbstractUIPlugin {
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+		ServiceTracker<ExtendedLogService, ExtendedLogService> logServiceTracker = new ServiceTracker<ExtendedLogService,
+				ExtendedLogService>(
+				                 context, ExtendedLogService.class.getName(), null);
+				         logServiceTracker.open();
+				         ExtendedLogService logservice = logServiceTracker.getService();
+
+				         if (logservice != null) {
+				             logger = logservice.getLogger(EQUINOX_LOGGER_NAME);
+				         }
+
+	}
+
+	/**
+	 * @return logger
+	 */
+	public static Logger getLogger()
+	{
+	     return logger;
 	}
 
 	/*
--- plugins/diagram/com.isb.jvoice.diagram.core/src/com/isb/jvoice/diagram/core/features/CoreFeatureProvider.java
+++ plugins/diagram/com.isb.jvoice.diagram.core/src/com/isb/jvoice/diagram/core/features/CoreFeatureProvider.java
@@ -4,6 +4,10 @@ package com.isb.jvoice.diagram.core.features;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.equinox.log.Logger;
 import org.eclipse.graphiti.dt.IDiagramTypeProvider;
 import org.eclipse.graphiti.features.IAddFeature;
 import org.eclipse.graphiti.features.ICopyFeature;
@@ -36,8 +40,11 @@ import org.eclipse.graphiti.features.context.IResizeShapeContext;
 import org.eclipse.graphiti.features.context.IUpdateContext;
 import org.eclipse.graphiti.features.custom.ICustomFeature;
 import org.eclipse.graphiti.mm.pictograms.PictogramElement;
-import org.eclipse.graphiti.ui.features.DefaultFeatureProvider;
+import org.eclipse.graphiti.pattern.DefaultFeatureProviderWithPatterns;
+import org.eclipse.graphiti.pattern.IPattern;
+import org.osgi.service.log.LogService;
 
+import com.isb.jvoice.diagram.core.Activator;
 import com.isb.jvoice.diagram.core.features.editing.CreateConditionalFeature;
 import com.isb.jvoice.diagram.core.features.editing.CreateEndFeature;
 import com.isb.jvoice.diagram.core.features.editing.CreateInitialFeature;
@@ -47,18 +54,40 @@ import com.isb.jvoice.diagram.core.features.editing.CreateSubFlowFeature;
 
 
 
-public class CoreFeatureProvider extends DefaultFeatureProvider{
+public class CoreFeatureProvider extends DefaultFeatureProviderWithPatterns{
+	public static final String PATTERN_EXTENSIONS_ID = "com.isb.jvoice.diagram.core.patterContributor"; //$NON-NLS-1$
 
 
 	public CoreFeatureProvider(IDiagramTypeProvider dtp) 
 	{
 		super(dtp);
+		IConfigurationElement[] config = Platform.getExtensionRegistry().getConfigurationElementsFor(PATTERN_EXTENSIONS_ID);
+
+		try {
+				for (IConfigurationElement configEl : config) 
+				{
+					Object o;				
+						o = configEl.createExecutableExtension("patterncontributorClass");				
+					if (o instanceof IPattern) {
+						IPattern patternContributor = (IPattern) o;
+						addPattern(patternContributor);
+					}
+				}
+			} catch (CoreException e) 
+			{
+				Logger logger = Activator.getLogger();
+			    if (logger != null)
+			    {
+			    	logger.log(LogService.LOG_ERROR, "CoreFeatureProvider::CoreFeatureProvider, Error adding patterns",e);
+			    }
+			}
+
+
 	}
 	
 	@Override
 	public IAddFeature getAddFeature(IAddContext context) 
 	{
-
 		return super.getAddFeature(context);
 	}
 	
@@ -94,8 +123,7 @@ public class CoreFeatureProvider extends DefaultFeatureProvider{
 	@Override
 	public IUpdateFeature getUpdateFeature(IUpdateContext context) 
 	{
-		PictogramElement pictogramElement = context.getPictogramElement();
-		
+		PictogramElement pictogramElement = context.getPictogramElement();		
 		return null;
 	}
 	
