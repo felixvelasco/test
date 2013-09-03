723aa57473095e4b09232445fa8cedf53f5d4986
Luis Porras <luis.escudero@servexternos.isban.es>
2013-09-03 10:13:03 +0200

    cambiado para que muestre solo el nombre del elemento en la vista

--- plugins/model/com.vectorsf.jvoice.base.model.edit/src/com/vectorsf/jvoice/base/provider/ext/ExtBaseItemProviderAdapterFactory.java
+++ plugins/model/com.vectorsf.jvoice.base.model.edit/src/com/vectorsf/jvoice/base/provider/ext/ExtBaseItemProviderAdapterFactory.java
@@ -4,14 +4,43 @@ import org.eclipse.emf.common.notify.Adapter;
 
 import com.vectorsf.jvoice.model.base.provider.BaseItemProviderAdapterFactory;
 
-public class ExtBaseItemProviderAdapterFactory extends BaseItemProviderAdapterFactory {
+public class ExtBaseItemProviderAdapterFactory extends
+		BaseItemProviderAdapterFactory {
 
 	@Override
 	public Adapter createJVProjectAdapter() {
 		if (jvProjectItemProvider == null) {
-			jvProjectItemProvider = new ExtJVProjectItemProvider(this);
+			jvProjectItemProvider =  new ExtJVProjectItemProvider(this);
 		}
 
 		return jvProjectItemProvider;
 	}
+	
+	@Override
+	public Adapter createJVPackageAdapter() {
+		if (jvPackageItemProvider == null) {
+			jvPackageItemProvider = new ExtJVPackageItemProvider(this);
+		}
+
+		return jvPackageItemProvider;
+	}
+	
+	@Override
+	public Adapter createNamedElementAdapter() {
+		if (namedElementItemProvider == null) {
+			namedElementItemProvider = new ExtNamedElementItemProvider(this);
+		}
+
+		return namedElementItemProvider;
+	}
+	
+	@Override
+	public Adapter createConfigurationAdapter() {
+		if (configurationItemProvider == null) {
+			configurationItemProvider = new ExtConfigurationItemProvider(this);
+		}
+
+		return configurationItemProvider;
+	}
+	
 }
