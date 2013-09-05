0d299da923bf3a884028fe1c2bffac8e1e4a4cc0
Felix Velasco <fvelascv@isban.es>
2013-09-05 18:48:57 +0200

    * Minimum size enforced * Update label from model * Allow direct editing

--- plugins/diagram/com.isb.jvoice.diagram.core/src/com/isb/jvoice/diagram/core/pattern/SwitchPattern.java
+++ plugins/diagram/com.isb.jvoice.diagram.core/src/com/isb/jvoice/diagram/core/pattern/SwitchPattern.java
@@ -3,7 +3,11 @@ package com.isb.jvoice.diagram.core.pattern;
 import org.eclipse.graphiti.features.IReason;
 import org.eclipse.graphiti.features.context.IAddContext;
 import org.eclipse.graphiti.features.context.ICreateContext;
+import org.eclipse.graphiti.features.context.IDirectEditingContext;
+import org.eclipse.graphiti.features.context.IResizeShapeContext;
+import org.eclipse.graphiti.features.context.impl.ResizeShapeContext;
 import org.eclipse.graphiti.features.impl.Reason;
+import org.eclipse.graphiti.func.IDirectEditing;
 import org.eclipse.graphiti.mm.algorithms.GraphicsAlgorithm;
 import org.eclipse.graphiti.mm.algorithms.Polygon;
 import org.eclipse.graphiti.mm.algorithms.Rectangle;
@@ -26,11 +30,13 @@ import com.vectorsf.jvoice.model.operations.SwitchState;
 
 public class SwitchPattern extends IdPattern {
 
+	private static final int MIN_WIDTH = 120;
+	private static final int MIN_HEIGHT = 80;
 	private static final String ID_SWITCH_PREFIX = "switchState_";
 	private static final String ID_NAME_TEXT = ID_SWITCH_PREFIX + "nameText";
 	private static final String ID_OUTER_RECTANGLE = ID_SWITCH_PREFIX + "outerRectangle";
 	private static final String ID_MAIN_RECTANGLE = ID_SWITCH_PREFIX + "mainRectangle";
-	private static final String ID_CASE_POLIGON = ID_SWITCH_PREFIX + "casePolygon";
+	private static final String ID_CASE_POLYGON = ID_SWITCH_PREFIX + "casePolygon";
 
 	@Override
 	public boolean canAdd(IAddContext context) {
@@ -48,12 +54,12 @@ public class SwitchPattern extends IdPattern {
 		ContainerShape outerContainerShape = peCreateService.createContainerShape(targetDiagram, true);
 		Rectangle outerRectangle = gaService.createInvisibleRectangle(outerContainerShape);
 		setId(outerRectangle, ID_OUTER_RECTANGLE);
-		gaService.setLocationAndSize(outerRectangle, context.getX(), context.getY(), context.getWidth(),
-				context.getHeight());
+		gaService.setLocationAndSize(outerRectangle, context.getX(), context.getY(),
+				Math.max(MIN_WIDTH, context.getWidth()), Math.max(MIN_HEIGHT, context.getHeight()));
 
 		// Default end point
 		Polygon endPoint = gaService.createPlainPolygon(outerRectangle, new int[] { 0, 0, 20, 40, 40, 0 });
-		setId(endPoint, ID_CASE_POLIGON);
+		setId(endPoint, ID_CASE_POLYGON);
 		setIndex(endPoint, 0);
 		endPoint.setFilled(true);
 		gaService.setRenderingStyle(endPoint, SwitchStatePredefinedColoredAreas.getGreenWhiteAdaptions());
@@ -80,6 +86,21 @@ public class SwitchPattern extends IdPattern {
 	}
 
 	@Override
+	public void resizeShape(IResizeShapeContext context) {
+		if (context.getWidth() < MIN_WIDTH || context.getHeight() < MIN_HEIGHT) {
+			ResizeShapeContext context2 = new ResizeShapeContext(context.getShape());
+			context2.setWidth(Math.max(MIN_WIDTH, context.getWidth()));
+			context2.setHeight(Math.max(MIN_HEIGHT, context.getHeight()));
+			context2.setX(context.getX());
+			context2.setY(context.getY());
+			context2.setDirection(context.getDirection());
+			super.resizeShape(context2);
+		} else {
+			super.resizeShape(context);
+		}
+	}
+
+	@Override
 	protected boolean layout(IdLayoutContext context, String id) {
 		boolean changesDone = false;
 
@@ -87,15 +108,20 @@ public class SwitchPattern extends IdPattern {
 
 		if (id.equals(ID_MAIN_RECTANGLE) || id.equals(ID_NAME_TEXT)) {
 			GraphicsAlgorithm ga = context.getGraphicsAlgorithm();
-			Graphiti.getGaService().setLocationAndSize(ga, 0, 0, outerRectangle.getWidth(),
-					outerRectangle.getHeight() - 20);
-			changesDone = true;
+			if (ga.getWidth() != outerRectangle.getWidth() || ga.getHeight() != outerRectangle.getHeight() - 20) {
+				Graphiti.getGaService().setLocationAndSize(ga, 0, 0, outerRectangle.getWidth(),
+						outerRectangle.getHeight() - 20);
+				changesDone = true;
+			}
 		}
-		if (id.equals(ID_CASE_POLIGON)) {
+		if (id.equals(ID_CASE_POLYGON)) {
 			GraphicsAlgorithm ga = context.getGraphicsAlgorithm();
-			System.out.println(getIndex(ga));
-			Graphiti.getGaService().setLocationAndSize(ga, outerRectangle.getWidth() / 2 - 20,
-					outerRectangle.getHeight() - 40, 40, 40);
+			int x = outerRectangle.getWidth() / 2 - 20;
+			int y = outerRectangle.getHeight() - 40;
+			if (ga.getX() != x || ga.getY() != y) {
+				Graphiti.getGaService().setLocationAndSize(ga, x, y, 40, 40);
+				changesDone = true;
+			}
 		}
 
 		return changesDone;
@@ -127,11 +153,24 @@ public class SwitchPattern extends IdPattern {
 
 	@Override
 	protected IReason updateNeeded(IdUpdateContext context, String id) {
+		if (id.equals(ID_NAME_TEXT)) {
+			SwitchState ss = (SwitchState) getBusinessObjectForPictogramElement(context.getPictogramElement());
+			Text text = (Text) context.getGraphicsAlgorithm();
+			if (!text.getValue().equals(ss.getName())) {
+				return Reason.createTrueReason();
+			}
+		}
 		return Reason.createFalseReason();
 	}
 
 	@Override
 	protected boolean update(IdUpdateContext context, String id) {
+		if (id.equals(ID_NAME_TEXT)) {
+			SwitchState ss = (SwitchState) getBusinessObjectForPictogramElement(context.getPictogramElement());
+			Text text = (Text) context.getGraphicsAlgorithm();
+			text.setValue(ss.getName());
+			return true;
+		}
 		return false;
 	}
 
@@ -140,4 +179,22 @@ public class SwitchPattern extends IdPattern {
 		return mainBusinessObject instanceof SwitchState;
 	}
 
+	@Override
+	protected boolean canDirectEdit(IDirectEditingContext context, String id) {
+		return id.equals(ID_NAME_TEXT);
+	}
+
+	@Override
+	public int getEditingType() {
+		return IDirectEditing.TYPE_TEXT;
+	}
+
+	@Override
+	protected void setValue(String value, IDirectEditingContext context, String id) {
+		if (id.equals(ID_NAME_TEXT)) {
+			SwitchState ss = (SwitchState) getBusinessObjectForPictogramElement(context.getPictogramElement());
+			ss.setName(value);
+			updatePictogramElement(context.getPictogramElement());
+		}
+	}
 }
