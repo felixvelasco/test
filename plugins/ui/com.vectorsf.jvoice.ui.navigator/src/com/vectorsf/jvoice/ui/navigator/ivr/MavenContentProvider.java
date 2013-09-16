6ad8856458bbe39e39c264a3213e5bdd24c270c8
Luis Porras <luis.escudero@servexternos.isban.es>
2013-09-16 16:23:58 +0200

    Ahora la vista muestra el paquete de recursos

--- plugins/ui/com.vectorsf.jvoice.ui.navigator/src/com/vectorsf/jvoice/ui/navigator/ivr/MavenContentProvider.java
+++ plugins/ui/com.vectorsf.jvoice.ui.navigator/src/com/vectorsf/jvoice/ui/navigator/ivr/MavenContentProvider.java
@@ -1,11 +1,15 @@
 package com.vectorsf.jvoice.ui.navigator.ivr;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.internal.ui.navigator.JavaNavigatorContentProvider;
@@ -16,32 +20,52 @@ import com.vectorsf.jvoice.model.base.JVProject;
 @SuppressWarnings("restriction")
 public class MavenContentProvider extends JavaNavigatorContentProvider {
 
-	private IPath mavenPath = new Path("org.eclipse.m2e.MAVEN2_CLASSPATH_CONTAINER");
+	private IPath mavenPath = new Path(
+			"org.eclipse.m2e.MAVEN2_CLASSPATH_CONTAINER");
 
 	@Override
 	public Object[] getChildren(Object object) {
 		if (object instanceof JVProject) {
-			IProject prj = ResourcesPlugin.getWorkspace().getRoot().getProject(((JVProject) object).getName());
+			IProject prj = ResourcesPlugin.getWorkspace().getRoot()
+					.getProject(((JVProject) object).getName());
 			IJavaProject project = JavaCore.create(prj);
 
 			if (!project.getProject().isOpen()) {
 				return NO_CHILDREN;
 			}
-
+			boolean hayElementos = false;
+			List<Object> result = new ArrayList<Object>();
 			try {
 				IClasspathEntry[] rawClasspath = project.getRawClasspath();
+
 				for (int i = 0; i < rawClasspath.length; i++) {
 					IClasspathEntry classpathEntry = rawClasspath[i];
-					System.out.println(classpathEntry.getPath());
 					if (classpathEntry.getEntryKind() == IClasspathEntry.CPE_CONTAINER
 							&& classpathEntry.getPath().equals(mavenPath)) {
-						return new Object[] { new ClassPathContainer(project, classpathEntry) };
+						result.add(new ClassPathContainer(project,
+								classpathEntry));
+						hayElementos = true;
+
+					} else if (classpathEntry.getEntryKind() == IClasspathEntry.CPE_SOURCE) {
+
+						IPackageFragmentRoot root = project
+								.findPackageFragmentRoot(classpathEntry
+										.getPath());
+						if (root != null) {
+							result.add(root);
+							hayElementos = true;
+						}
+
 					}
 				}
 			} catch (JavaModelException e) {
 				e.printStackTrace();
 			}
-			return new Object[0];
+			if (hayElementos) {
+				return result.toArray();
+			} else {
+				return new Object[0];
+			}
 		}
 
 		return super.getChildren(object);
