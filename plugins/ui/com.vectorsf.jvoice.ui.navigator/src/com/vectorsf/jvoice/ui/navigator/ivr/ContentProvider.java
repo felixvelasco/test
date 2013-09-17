79733ee63f88c6159bd9a1f28c3404e743ce7af0
Felix Velasco <fvelascv@isban.es>
2013-09-17 08:57:27 +0200

    Configure test dependencies in the test, not in the code

--- plugins/ui/com.vectorsf.jvoice.ui.navigator/src/com/vectorsf/jvoice/ui/navigator/ivr/ContentProvider.java
+++ plugins/ui/com.vectorsf.jvoice.ui.navigator/src/com/vectorsf/jvoice/ui/navigator/ivr/ContentProvider.java
@@ -1,70 +1,11 @@
 package com.vectorsf.jvoice.ui.navigator.ivr;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
 import org.eclipse.emf.edit.ui.provider.AdapterFactoryContentProvider;
-import org.eclipse.jface.viewers.ITreeContentProvider;
-import org.eclipse.jface.viewers.Viewer;
 
-import com.vectorsf.jvoice.model.base.JVProject;
-
-public class ContentProvider implements ITreeContentProvider {
-
-	AdapterFactoryContentProvider afcp;
+public class ContentProvider extends AdapterFactoryContentProvider {
 
 	public ContentProvider() {
-		ComposedAdapterFactory factory = new ComposedAdapterFactory(ComposedAdapterFactory.Descriptor.Registry.INSTANCE);
-		afcp = new AdapterFactoryContentProvider(factory);
+		super(new ComposedAdapterFactory(ComposedAdapterFactory.Descriptor.Registry.INSTANCE));
 	}
-
-	@Override
-	public void dispose() {
-		afcp.dispose();
-	}
-
-	@Override
-	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-		afcp.inputChanged(viewer, oldInput, newInput);
-	}
-
-	@Override
-	public Object[] getElements(Object inputElement) {
-		return afcp.getElements(inputElement);
-	}
-
-	@Override
-	public Object[] getChildren(Object parentElement) {
-		if (parentElement instanceof JVProject) {
-			List<Object> children = new ArrayList<>();
-			Collections.addAll(children, afcp.getChildren(parentElement));
-
-			// try {
-			// for(int i=0; i<jProject.getRawClasspath().length; i++){
-			// IClasspathEntry miclase = jProject.getRawClasspath()[i];
-			// children.add(miclase);
-			// }
-			// //children.add(jProject.getRawClasspath());
-			// } catch (JavaModelException e) {
-			// // TODO Auto-generated catch block
-			// e.printStackTrace();
-			// }
-			return children.toArray();
-		} else {
-			return afcp.getChildren(parentElement);
-		}
-	}
-
-	@Override
-	public Object getParent(Object element) {
-		return afcp.getParent(element);
-	}
-
-	@Override
-	public boolean hasChildren(Object element) {
-		return afcp.hasChildren(element) || true;
-	}
-
 }
