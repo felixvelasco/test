4570982492615516276bf022ae06246827fc5852
Esther Mateos Dominguez <esther.mateos@servexternos.isban.es>
2013-09-25 16:31:31 +0200

    Opciones de Open y doble click

--- plugins/ui/com.vectorsf.jvoice.ui.navigator/src/com/vectorsf/jvoice/ui/navigator/CommonActionProviderIVR.java
+++ plugins/ui/com.vectorsf.jvoice.ui.navigator/src/com/vectorsf/jvoice/ui/navigator/CommonActionProviderIVR.java
@@ -3,16 +3,24 @@
  */
 package com.vectorsf.jvoice.ui.navigator;
 
-import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.mapping.ResourceMapping;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.jface.action.GroupMarker;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IActionBars;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.ide.IDE;
-import org.eclipse.ui.navigator.CommonActionProvider;
+import org.eclipse.ui.actions.OpenFileAction;
+import org.eclipse.ui.actions.OpenWithMenu;
+import org.eclipse.ui.internal.navigator.AdaptabilityUtility;
+import org.eclipse.ui.internal.navigator.resources.actions.OpenActionProvider;
+import org.eclipse.ui.internal.navigator.resources.plugin.WorkbenchNavigatorMessages;
+import org.eclipse.ui.navigator.ICommonActionConstants;
+import org.eclipse.ui.navigator.ICommonActionExtensionSite;
+import org.eclipse.ui.navigator.ICommonMenuConstants;
+import org.eclipse.ui.navigator.ICommonViewerWorkbenchSite;
 
 import com.vectorsf.jvoice.model.base.JVBean;
 
@@ -20,39 +28,91 @@ import com.vectorsf.jvoice.model.base.JVBean;
  * @author xIS14487
  * 
  */
-public class CommonActionProviderIVR extends CommonActionProvider {
+public class CommonActionProviderIVR extends OpenActionProvider {
+
+	private OpenFileAction openFileAction;
+
+	private ICommonViewerWorkbenchSite viewSite = null;
+	private boolean contribute = false;
 
 	@Override
-	public void fillActionBars(IActionBars actionBars) {
-		super.fillActionBars(actionBars);
+	public void init(ICommonActionExtensionSite aConfig) {
+		if (aConfig.getViewSite() instanceof ICommonViewerWorkbenchSite) {
+			viewSite = (ICommonViewerWorkbenchSite) aConfig.getViewSite();
+			openFileAction = new OpenFileAction(viewSite.getPage());
+			contribute = true;
+		}
+	}
+
+	@Override
+	public void fillContextMenu(IMenuManager aMenu) {
+		if (!contribute || getContext().getSelection().isEmpty()) {
+			return;
+		}
+
 		IStructuredSelection selection = (IStructuredSelection) getContext()
 				.getSelection();
 
-		for (Object o : selection.toArray()) {
-			if (o instanceof JVBean) {
+		openFileAction.selectionChanged(selection);
+		if (openFileAction.isEnabled()) {
+			aMenu.insertAfter(ICommonMenuConstants.GROUP_OPEN, openFileAction);
+		}
+		addOpenWithMenu(aMenu);
+	}
 
-				// re-target the double-click open action.
-				IWorkbenchPage page = PlatformUI.getWorkbench()
-						.getActiveWorkbenchWindow().getActivePage();
-				IFile file = (IFile) Platform.getAdapterManager().getAdapter(o,
-						IFile.class);
-
-				try {
-					IDE.openEditor(page, file);
-
-				} catch (PartInitException e) {
-					try {
-						throw new ExecutionException("Error opening "
-								+ ((JVBean) o).getName(), e);
-					} catch (ExecutionException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-				}
-
-			}
+	@Override
+	public void fillActionBars(IActionBars theActionBars) {
+		if (!contribute) {
+			return;
+		}
+		IStructuredSelection selection = (IStructuredSelection) getContext()
+				.getSelection();
+		if (selection.size() == 1
+				&& (selection.getFirstElement() instanceof IFile || selection
+						.getFirstElement() instanceof JVBean)) {
+			openFileAction.selectionChanged(selection);
+			theActionBars.setGlobalActionHandler(ICommonActionConstants.OPEN,
+					openFileAction);
 		}
 
+	}
+
+	private void addOpenWithMenu(IMenuManager aMenu) {
+		IStructuredSelection ss = (IStructuredSelection) getContext()
+				.getSelection();
+
+		if (ss == null || ss.size() != 1) {
+			return;
+		}
+
+		Object o = ss.getFirstElement();
+
+		// first try IResource
+		IAdaptable openable = (IAdaptable) AdaptabilityUtility.getAdapter(o,
+				IResource.class);
+		// otherwise try ResourceMapping
+		if (openable == null) {
+			openable = (IAdaptable) AdaptabilityUtility.getAdapter(o,
+					ResourceMapping.class);
+		} else if (((IResource) openable).getType() != IResource.FILE) {
+			openable = null;
+		}
+
+		if (openable != null) {
+			// Create a menu flyout.
+			IMenuManager submenu = new MenuManager(
+					WorkbenchNavigatorMessages.OpenActionProvider_OpenWithMenu_label,
+					ICommonMenuConstants.GROUP_OPEN_WITH);
+			submenu.add(new GroupMarker(ICommonMenuConstants.GROUP_TOP));
+			submenu.add(new OpenWithMenu(viewSite.getPage(), openable));
+			submenu.add(new GroupMarker(ICommonMenuConstants.GROUP_ADDITIONS));
+
+			// Add the submenu.
+			if (submenu.getItems().length > 2 && submenu.isEnabled()) {
+				aMenu.appendToGroup(ICommonMenuConstants.GROUP_OPEN_WITH,
+						submenu);
+			}
+		}
 	}
 
 }
