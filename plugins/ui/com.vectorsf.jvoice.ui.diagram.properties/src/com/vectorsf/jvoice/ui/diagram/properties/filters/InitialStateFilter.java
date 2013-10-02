91eb05f6b6facec6bc2c196665c2be1a239874f4
Esther Mateos Dominguez <esther.mateos@servexternos.isban.es>
2013-10-02 12:47:55 +0200

    Propiedades para el estado Inicial

---
+++ plugins/ui/com.vectorsf.jvoice.ui.diagram.properties/src/com/vectorsf/jvoice/ui/diagram/properties/filters/InitialStateFilter.java
@@ -0,0 +1,24 @@
+package com.vectorsf.jvoice.ui.diagram.properties.filters;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.graphiti.mm.pictograms.PictogramElement;
+import org.eclipse.graphiti.services.Graphiti;
+import org.eclipse.graphiti.ui.platform.AbstractPropertySectionFilter;
+
+import com.vectorsf.jvoice.model.operations.InitialState;
+
+public class InitialStateFilter extends AbstractPropertySectionFilter {
+	
+    @Override
+    protected boolean accept(PictogramElement pe) {
+        EObject eObject =
+            Graphiti.getLinkService()
+            .getBusinessObjectForLinkedPictogramElement(pe);
+        if (eObject instanceof InitialState) {
+            return true;
+        }
+        return false;
+    }
+
+
+}
