e0c6cfd38e84930bf2f7979cb4f18d55f024d2c5
Luis Porras <luis.escudero@servexternos.isban.es>
2013-10-07 11:47:30 +0200

    creados after y before

--- tests/com.vectorsf.jvoice.ui.diagram.properties.test/src/com/vectorsf/jvoice/ui/diagram/properties/test/PropertiesDiagramTest.java
+++ tests/com.vectorsf.jvoice.ui.diagram.properties.test/src/com/vectorsf/jvoice/ui/diagram/properties/test/PropertiesDiagramTest.java
@@ -3,10 +3,94 @@
  */
 package com.vectorsf.jvoice.ui.diagram.properties.test;
 
+import static org.hamcrest.Matchers.arrayContaining;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.fail;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
+import org.eclipse.swtbot.eclipse.gef.finder.SWTGefBot;
+import org.eclipse.swtbot.eclipse.gef.finder.widgets.SWTBotGefEditor;
+import org.eclipse.swtbot.eclipse.gef.finder.widgets.SWTBotGefViewer;
+import org.eclipse.swtbot.swt.finder.finders.UIThreadRunnable;
+import org.eclipse.swtbot.swt.finder.results.VoidResult;
+import org.eclipse.ui.PlatformUI;
+import org.hamcrest.Matcher;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import com.vectorsf.jvoice.ui.diagram.properties.test.SWTBotHelper;
+
 /**
  * @author xIS16396
  *
  */
 public class PropertiesDiagramTest {
 
+	private static final int SMALL_SLEEP = 200;
+	private static final int LARGE_SLEEP = 100;
+	private static final String NAVIGATOR_ID = "com.vectorsf.jvoice.ui.navigator.ViewIVR";
+	private static final String DIAGRAM_ID = "org.eclipse.graphiti.ui.editor.DiagramEditor";
+	protected static SWTGefBot bot = new SWTGefBot();
+	private SWTBotView view;
+	private SWTBotGefEditor editor;
+	private SWTBotGefViewer gefViewer;
+	@SuppressWarnings("unchecked")
+	private final Matcher<Object[]> arrayContainingTextOne = arrayContaining(hasProperty(
+			"text", is("one")));
+	@SuppressWarnings("unchecked")
+	private final Matcher<Object[]> arrayContainingTextTwo = arrayContaining(hasProperty(
+			"text", is("two")));
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setClassUp() throws Exception {
+		SWTBotHelper.closeWelcomeView(bot);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		UIThreadRunnable.syncExec(new VoidResult() {
+			@Override
+			public void run() {
+				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell()
+						.forceActive();
+			}
+		});
+
+		SWTBotHelper.openView(bot, "IVR", "Navigator IVR");
+
+		view = bot.viewById(NAVIGATOR_ID);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		bot.viewById(NAVIGATOR_ID).close();
+		if (editor != null) {
+			editor.close();
+		}
+
+		for (IProject project : ResourcesPlugin.getWorkspace().getRoot()
+				.getProjects()) {
+
+			try {
+				SWTBotHelper.deleteProject(project);
+			} catch (CoreException ce) {
+				ce.printStackTrace();
+				fail(ce.getMessage());
+			}
+		}
+	}
 }
