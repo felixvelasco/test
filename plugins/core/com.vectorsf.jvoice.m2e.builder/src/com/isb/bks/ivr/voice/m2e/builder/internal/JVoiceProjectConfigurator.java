cfafbbfd3dddbd4eea08c30c3fc9fe4e6d9d0bdd
Felix Velasco <fvelascov@ext.vectorsf.com>
2013-10-16 15:17:10 +0200

    Configure m2e build participant

--- plugins/core/com.vectorsf.jvoice.m2e.builder/src/com/isb/bks/ivr/voice/m2e/builder/internal/JVoiceProjectConfigurator.java
+++ plugins/core/com.vectorsf.jvoice.m2e.builder/src/com/isb/bks/ivr/voice/m2e/builder/internal/JVoiceProjectConfigurator.java
@@ -6,13 +6,11 @@ import org.eclipse.m2e.core.project.IMavenProjectFacade;
 import org.eclipse.m2e.core.project.configurator.AbstractBuildParticipant;
 import org.eclipse.m2e.jdt.AbstractSourcesGenerationProjectConfigurator;
 
-public class JVoiceProjectConfigurator extends
-	AbstractSourcesGenerationProjectConfigurator {
+public class JVoiceProjectConfigurator extends AbstractSourcesGenerationProjectConfigurator {
 
-    @Override
-    public AbstractBuildParticipant getBuildParticipant(
-	    IMavenProjectFacade projectFacade, MojoExecution execution,
-	    IPluginExecutionMetadata executionMetadata) {
-	return new JVoiceBuildParticipant(execution);
-    }
+	@Override
+	public AbstractBuildParticipant getBuildParticipant(IMavenProjectFacade projectFacade, MojoExecution execution,
+			IPluginExecutionMetadata executionMetadata) {
+		return new JVoiceBuildParticipant(execution);
+	}
 }
