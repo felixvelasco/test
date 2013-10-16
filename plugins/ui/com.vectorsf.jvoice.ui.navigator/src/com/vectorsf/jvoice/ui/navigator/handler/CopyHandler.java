7072ad0be60cd4b00c93e0f67afd3df0844b7ba2
Luis Porras <luis.escudero@servexternos.isban.es>
2013-10-16 14:48:51 +0200

    Modificado el Copy del navegador, ya no se activa siempre y ahora permite copiados múltiples mientras sean del mismo tipo

--- plugins/ui/com.vectorsf.jvoice.ui.navigator/src/com/vectorsf/jvoice/ui/navigator/handler/CopyHandler.java
+++ plugins/ui/com.vectorsf.jvoice.ui.navigator/src/com/vectorsf/jvoice/ui/navigator/handler/CopyHandler.java
@@ -1,18 +1,75 @@
 package com.vectorsf.jvoice.ui.navigator.handler;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.expressions.IEvaluationContext;
+import org.eclipse.jface.util.LocalSelectionTransfer;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.Transfer;
 
-import com.vectorsf.jvoice.ui.navigator.util.UtilHandler;
+import com.vectorsf.jvoice.model.base.JVBean;
+import com.vectorsf.jvoice.model.base.JVPackage;
 
 public class CopyHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 
-		UtilHandler.Copiar(event);
+		Object o = ((IEvaluationContext) event.getApplicationContext())
+				.getDefaultVariable();
+
+		if (o instanceof List<?>) {
+			Object[] selection = ((List<?>) o).toArray();
+			LocalSelectionTransfer.getTransfer().setSelection(
+					new StructuredSelection(selection));
+			Clipboard clipboard = new Clipboard(null);
+			clipboard.setContents(new Object[] { new Object() },
+					new Transfer[] { LocalSelectionTransfer.getTransfer() });
+
+		}
 
 		return null;
 	}
+
+	@Override
+	public void setEnabled(Object evaluationContext) {
+		setBaseEnabled(getValidTarget(evaluationContext));
+	}
+
+	@SuppressWarnings("unchecked")
+	private boolean getValidTarget(Object target) {
+		Object def = ((IEvaluationContext) target).getDefaultVariable();
+		if (def instanceof Collection<?>) {
+			boolean paquete = false;
+			boolean bean = false;
+			Collection<Object> colection = (Collection<Object>) def;
+
+			for (int i = 0; i < colection.toArray().length; i++) {
+				Object o = colection.toArray()[i];
+				if (o instanceof JVPackage) {
+					if (bean) {
+						return false;
+					} else {
+						paquete = true;
+					}
+				} else if (o instanceof JVBean) {
+					if (paquete) {
+						return false;
+					} else {
+						bean = true;
+					}
+				} else {
+					return false;
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+
 }
