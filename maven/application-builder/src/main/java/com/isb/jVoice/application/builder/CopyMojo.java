3875028c6d5311c4f762b368f02dda7862ff33c4
Esther Mateos Dominguez <esther.mateos@servexternos.isban.es>
2013-10-24 17:37:53 +0200

    Compilador para copiar los XML de las dependencias en el proyecto de la aplicacion

---
+++ maven/application-builder/src/main/java/com/isb/jVoice/application/builder/CopyMojo.java
@@ -0,0 +1,170 @@
+package com.isb.jVoice.application.builder;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.MavenProjectHelper;
+
+
+/**
+ * Goal which touches a timestamp file.
+ * 
+ * @goal copyXML
+ * 
+ * @phase process-resources
+ * 
+ * @requiresDependencyResolution compile+runtime
+ */
+public class CopyMojo extends AbstractMojo {
+	
+	private static final String SEPARATOR = "/";
+	private static final String DOT = ".";
+	private static final String JVOICES = "jVoice";
+	private static final String DESTINO = "/src/main/webapp/WEB-INF/flows";
+	
+	/**
+	 * Location of the target directory.
+	 * 
+	 * @parameter expression="${project.build.directory}"
+	 */
+	private File outputDirectory;
+
+	/**
+	 * Location of the source directory.
+	 * 
+	 * @parameter expression="${basedir}/src/main/resources/jv"
+	 */
+	private File sourceDirectory;
+
+	/**
+	 * A set of patterns matching files from the sourceDirectory that should be processed as grammars.
+	 * 
+	 * @parameter
+	 */
+	protected Set<String> includes = new HashSet<>();
+
+	/**
+	 * A set of exclude patterns.
+	 * 
+	 * @parameter
+	 */
+	protected Set<String> excludes = new HashSet<>();
+
+	/**
+	 * The Maven Project Object
+	 * 
+	 * @parameter expression="${project}"
+	 * @required
+	 */
+	protected MavenProject project;
+
+	/**
+	 * The maven project's helper.
+	 * 
+	 * @component role="org.apache.maven.project.MavenProjectHelper"
+	 * @required
+	 * @readonly
+	 */
+	private MavenProjectHelper projectHelper;
+
+	/**
+	 * @parameter expression="${project.runtimeClasspathElements}"
+	 */
+	private List<String> runtimeClasspathElements;
+
+	private String findFullPath(String fileNameToSearch, File file)
+			throws IOException {
+		ZipInputStream zip = new ZipInputStream(new FileInputStream(file));
+		ZipEntry ze = null;
+		String ret = null;
+		while ((ze = zip.getNextEntry()) != null) {
+			String entryName = ze.getName();
+			int index = entryName.lastIndexOf(DOT);
+			if (index != -1) {
+				String entryNameDots = entryName.replace(
+						SEPARATOR, DOT);
+				if (entryNameDots.endsWith(fileNameToSearch)
+						&& entryNameDots.contains(JVOICES + DOT)) {
+					ret = entryName.substring(entryName.indexOf(SEPARATOR)+1, entryName.length());
+					ZipFile zipFile = new ZipFile(file);					
+                    InputStream inputStream = zipFile.getInputStream(ze);
+                    copyFile(inputStream, ret);
+                    zipFile.close();
+				}
+			}
+		}
+		zip.close();
+		return ret;
+	}
+	
+	@Override
+	public void execute() throws MojoExecutionException {
+		
+		for (String element : runtimeClasspathElements) {
+
+				if (new File(element).toURI().toString().endsWith(".jar")) {
+					File file = new File(element);
+					try {
+						findFullPath(".xml",file);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					 
+				}
+		}	   
+	}
+
+	/**
+	 * @param name 
+	 * 
+	 */
+	protected void copyFile(InputStream in, String name) {
+		File ruta = new File (name);
+		String pathname= project.getBasedir()+DESTINO+SEPARATOR+name.replace(ruta.getName(),"").trim();
+		/*Comprobamos que exista el directorio base donde vamos a crear los XML
+		 *Si no existe, se crea.
+		 */
+		File f = new File(pathname);
+		if (!f.exists()) {
+			f.mkdirs();
+		}
+
+		File destino = new File(pathname+ruta.getName());
+
+		try {				
+			OutputStream out = new FileOutputStream(destino);
+		    byte[] buf = new byte[1024];
+		    int len;
+		     
+		    while ((len = in.read(buf)) > 0) {
+		    out.write(buf, 0, len);
+		    }
+		    
+		    in.close();
+		    out.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+
+}
