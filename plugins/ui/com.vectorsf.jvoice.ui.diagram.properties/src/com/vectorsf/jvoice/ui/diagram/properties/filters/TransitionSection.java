c798b2d5f79443803a3b47a27e7f3924d0fa8ab3
Luis Porras <luis.escudero@servexternos.isban.es>
2013-10-25 16:04:12 +0200

    Nose puede modificar el nombre de la transción en las propiedades de la flecha. Solucionado bug que mostraba el nombre de otra anterior si la seleccionada no tenía nombre

--- plugins/ui/com.vectorsf.jvoice.ui.diagram.properties/src/com/vectorsf/jvoice/ui/diagram/properties/filters/TransitionSection.java
+++ plugins/ui/com.vectorsf.jvoice.ui.diagram.properties/src/com/vectorsf/jvoice/ui/diagram/properties/filters/TransitionSection.java
@@ -1,21 +1,10 @@
 package com.vectorsf.jvoice.ui.diagram.properties.filters;
 
-import java.util.List;
-
-import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.transaction.RecordingCommand;
-import org.eclipse.emf.transaction.TransactionalEditingDomain;
-import org.eclipse.emf.transaction.util.TransactionUtil;
-import org.eclipse.graphiti.mm.algorithms.GraphicsAlgorithm;
-import org.eclipse.graphiti.mm.pictograms.Connection;
-import org.eclipse.graphiti.mm.pictograms.ConnectionDecorator;
 import org.eclipse.graphiti.mm.pictograms.PictogramElement;
 import org.eclipse.graphiti.services.Graphiti;
 import org.eclipse.graphiti.ui.platform.GFPropertySection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CLabel;
-import org.eclipse.swt.events.FocusEvent;
-import org.eclipse.swt.events.FocusListener;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.widgets.Composite;
@@ -33,60 +22,8 @@ public class TransitionSection extends GFPropertySection implements
 	private Text pathText;
 	private Text InitialState;
 	private Text FinalState;
-	private org.eclipse.graphiti.mm.algorithms.Text nameArrow;
-	private PictogramElement pe;
 
 	public TransitionSection() {}
-	
-	private FocusListener listenerIntentionName = new FocusListener(){
-
-		public void focusLost(FocusEvent e) {
-
-		// set the new name for the Intention
-		pe = getSelectedPictogramElement();
-		final Transition bimElement = (Transition) Graphiti.getLinkService().getBusinessObjectForLinkedPictogramElement(pe);
-
-		TransactionalEditingDomain editingDomain = TransactionUtil.getEditingDomain(bimElement);
-		
-		
-		editingDomain.getCommandStack().execute(new RecordingCommand(editingDomain) {
-			
-			@Override
-			protected void doExecute() {
-			
-	            List<?> lista = Graphiti.getLinkService().getPictogramElements(getDiagramTypeProvider().getDiagram(), bimElement);
-	        	for (Object object : lista) {            		
-	        		if(object instanceof Connection){
-	        		Connection connection = (Connection) object;
-	        				//Obtenemos la conexion de nuestra transacion y la recorremos.
-	                    	EList<ConnectionDecorator> liCD = connection.getConnectionDecorators();
-	                    	for (ConnectionDecorator connectionDecorator : liCD) 
-	                    	{
-	                    		/**Obtenemos los Graphics Algorithms, que en este caso son dos, 
-	                    		 * el texto y el arrow.
-	                    		 * Para obtener el nombre, comprobamos que el Graphics Algorithm sea Text,
-	                    		 * de ser asi lo modificamos.
-	                    		 * 	                    		**/
-	                    		GraphicsAlgorithm ga= connectionDecorator.getGraphicsAlgorithm();
-	                    		if (ga instanceof org.eclipse.graphiti.mm.algorithms.Text)
-	                    		{
-	                    			nameArrow = (org.eclipse.graphiti.mm.algorithms.Text)ga;
-	                    			nameArrow.setValue(nameText.getText());
-	                    			
-	                    		}
-	                    		
-	        				}
-	        		}
-				}//Fin del for.
-				
-			}
-		});
-
-		}
-		
-		@Override
-		public void focusGained(FocusEvent e) {}
-	};
 	
 	@Override
     public void createControls(Composite parent,
@@ -104,7 +41,8 @@ public class TransitionSection extends GFPropertySection implements
         data.right = new FormAttachment(100, 0);
         data.top = new FormAttachment(0, VSPACE);
         nameText.setLayoutData(data);
-        nameText.addFocusListener(listenerIntentionName);
+        nameText.setEditable(false);
+        nameText.setEnabled(false); 
      
         CLabel LabelName = factory.createCLabel(composite, "Name:");
         data = new FormData();
@@ -116,9 +54,9 @@ public class TransitionSection extends GFPropertySection implements
         //Path donde se encuentra el elemento
         pathText = factory.createText(composite, "");
         data = new FormData();
-        data.left = new FormAttachment(0, STANDARD_LABEL_WIDTH+10);
-        data.right = new FormAttachment(100, 0);
-        data.top = new FormAttachment(0, VSPACE+30);
+        data.left = new FormAttachment(nameText, 0,SWT.LEFT);
+	    data.right = new FormAttachment(nameText,0,SWT.RIGHT);
+	    data.top = new FormAttachment(nameText, 10);
         pathText.setLayoutData(data);
         pathText.setEditable(false);
         pathText.setEnabled(false);       
@@ -133,9 +71,9 @@ public class TransitionSection extends GFPropertySection implements
         //Estado inicial   
         InitialState = factory.createText(composite, "");
        	data = new FormData();
-       	data.left = new FormAttachment(0,STANDARD_LABEL_WIDTH+10);
-       	data.right = new FormAttachment(100, 0);
-       	data.top = new FormAttachment(0, VSPACE+60);
+       	data.left = new FormAttachment(pathText, 0,SWT.LEFT);
+	    data.right = new FormAttachment(pathText,0,SWT.RIGHT);
+	    data.top = new FormAttachment(pathText, 10);
        	InitialState.setLayoutData(data);
        	InitialState.setEditable(false);
        	InitialState.setEnabled(false); 
@@ -150,9 +88,9 @@ public class TransitionSection extends GFPropertySection implements
         //Estado final   
         FinalState = factory.createText(composite, "");
        	data = new FormData();
-       	data.left = new FormAttachment(0,STANDARD_LABEL_WIDTH+10);
-       	data.right = new FormAttachment(100, 0);
-       	data.top = new FormAttachment(0, VSPACE+90);
+       	data.left = new FormAttachment(InitialState, 0,SWT.LEFT);
+	    data.right = new FormAttachment(InitialState,0,SWT.RIGHT);
+	    data.top = new FormAttachment(InitialState, 10);
        	FinalState.setLayoutData(data);
        	FinalState.setEditable(false); 
        	FinalState.setEnabled(false); 
@@ -167,7 +105,6 @@ public class TransitionSection extends GFPropertySection implements
  
     @Override
     public void refresh() {
-    	nameText.removeFocusListener(listenerIntentionName);
         PictogramElement pe = getSelectedPictogramElement();
         if (pe != null) {
             Object bo = Graphiti.getLinkService()
@@ -175,44 +112,17 @@ public class TransitionSection extends GFPropertySection implements
             // the filter assured, that it is a EClass
             if (bo == null)
                 return;
-            String name = null;
-            //Vamos a obtener el nombre del GA
-            Transition tr = (Transition)bo;
-            List<?> li = Graphiti.getLinkService().getPictogramElements(getDiagramTypeProvider().getDiagram(), tr);
-        	for (Object object : li) {            		
-        		if(object instanceof Connection){
-        		Connection connection = (Connection) object;
-        				//Obtenemos la conexion de nuestra transacion y la recorremos.
-                    	EList<ConnectionDecorator> liCD = connection.getConnectionDecorators();
-                    	for (ConnectionDecorator connectionDecorator : liCD) 
-                    	{
-                    		/**Obtenemos los Graphics Algorithms, que en este caso son dos, 
-                    		 * el texto y el arrow.
-                    		 * Para obtener el nombre, comprobamos que el Graphics Algorithm sea Text,
-                    		 * de ser asi obtenemos el nombre.
-                    		**/
-                    		GraphicsAlgorithm ga= connectionDecorator.getGraphicsAlgorithm();
-                    		if (ga instanceof org.eclipse.graphiti.mm.algorithms.Text)
-                    		{
-                    			nameArrow = (org.eclipse.graphiti.mm.algorithms.Text)ga;
-                    			name = nameArrow.getValue().toString();                                
-                                nameText.setText(name == null ? "" : name);
-                    		}
-                    		
-        				}
-        		}
-			}//Fin del for.  
 
-            nameText.addFocusListener(listenerIntentionName);
+            Transition tr = (Transition)bo;
+            
+            nameText.setText(tr.getEventName());
+        	
             String path = (tr.eResource()).getURI().path().substring(9).toString();
             pathText.setText(path == null ? "" : path);
             String origen = tr.getSource().getName().toString();
             InitialState.setText(origen == null ? "" : origen);
             String destino = tr.getTarget().getName().toString();
             FinalState.setText(destino == null ? "" : destino);
-
         }
     }
-
-
 }
