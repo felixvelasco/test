39ee0f4d63abf15dc4c29fba1be13ab54e179268
Luis Porras <luis.escudero@servexternos.isban.es>
2013-10-29 17:36:29 +0100

    Quitado remove del cuadro de diálogo en el diagrama

--- plugins/diagram/com.vectorsf.jvoice.diagram.core/src/com/vectorsf/jvoice/diagram/core/editor/JVoiceDiagramBehavior.java
+++ plugins/diagram/com.vectorsf.jvoice.diagram.core/src/com/vectorsf/jvoice/diagram/core/editor/JVoiceDiagramBehavior.java
@@ -1,12 +1,35 @@
 package com.vectorsf.jvoice.diagram.core.editor;
 
+import java.util.List;
+
+import org.eclipse.draw2d.PositionConstants;
 import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.gef.editparts.ZoomManager;
+import org.eclipse.gef.ui.actions.ActionRegistry;
+import org.eclipse.gef.ui.actions.AlignmentAction;
+import org.eclipse.gef.ui.actions.DirectEditAction;
+import org.eclipse.gef.ui.actions.MatchHeightAction;
+import org.eclipse.gef.ui.actions.MatchWidthAction;
+import org.eclipse.gef.ui.actions.ToggleGridAction;
+import org.eclipse.gef.ui.actions.ZoomInAction;
+import org.eclipse.gef.ui.actions.ZoomOutAction;
+import org.eclipse.graphiti.features.IFeatureProvider;
+import org.eclipse.graphiti.features.ISaveImageFeature;
 import org.eclipse.graphiti.ui.editor.DefaultPersistencyBehavior;
 import org.eclipse.graphiti.ui.editor.DiagramBehavior;
 import org.eclipse.graphiti.ui.editor.IDiagramContainerUI;
+import org.eclipse.graphiti.ui.internal.action.CopyAction;
+import org.eclipse.graphiti.ui.internal.action.DeleteAction;
+import org.eclipse.graphiti.ui.internal.action.FeatureExecutionHandler;
+import org.eclipse.graphiti.ui.internal.action.PasteAction;
+import org.eclipse.graphiti.ui.internal.action.SaveImageAction;
+import org.eclipse.graphiti.ui.internal.action.ToggleContextButtonPadAction;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.ui.handlers.IHandlerService;
 
 import com.vectorsf.jvoice.core.uri.VegaXMLURIHandlerImpl;
 
+@SuppressWarnings("restriction")
 public class JVoiceDiagramBehavior extends DiagramBehavior {
 
 	public JVoiceDiagramBehavior(IDiagramContainerUI diagramContainer) {
@@ -24,5 +47,78 @@ public class JVoiceDiagramBehavior extends DiagramBehavior {
 	@Override
 	protected DefaultPersistencyBehavior createPersistencyBehavior() {
 		return new JVoicePersistencyBehavior(this);
+	}
+
+	@Override
+	protected void initActionRegistry(ZoomManager zoomManager) {
+		if (getParentPart() == null) {
+			return;
+		}
+		final ActionRegistry actionRegistry = getDiagramContainer()
+				.getActionRegistry();
+		@SuppressWarnings("unchecked")
+		final List<String> selectionActions = getDiagramContainer()
+				.getSelectionActions();
+
+		// register predefined actions (e.g. update, remove, delete, ...)
+
+		IAction action = new DeleteAction(getParentPart(),
+				getConfigurationProvider());
+		actionRegistry.registerAction(action);
+		selectionActions.add(action.getId());
+
+		action = new CopyAction(getParentPart(), getConfigurationProvider());
+		actionRegistry.registerAction(action);
+		selectionActions.add(action.getId());
+
+		action = new PasteAction(getParentPart(), getConfigurationProvider());
+		actionRegistry.registerAction(action);
+		selectionActions.add(action.getId());
+
+		IFeatureProvider fp = getConfigurationProvider()
+				.getDiagramTypeProvider().getFeatureProvider();
+		if (fp != null) {
+			ISaveImageFeature sf = fp.getSaveImageFeature();
+
+			if (sf != null) {
+				action = new SaveImageAction(this, getConfigurationProvider());
+				actionRegistry.registerAction(action);
+				selectionActions.add(action.getId());
+			}
+		}
+
+		registerAction(new ZoomInAction(zoomManager));
+		registerAction(new ZoomOutAction(zoomManager));
+		registerAction(new DirectEditAction(getParentPart()));
+
+		registerAction(new AlignmentAction(getParentPart(),
+				PositionConstants.LEFT));
+		registerAction(new AlignmentAction(getParentPart(),
+				PositionConstants.RIGHT));
+		registerAction(new AlignmentAction(getParentPart(),
+				PositionConstants.TOP));
+		registerAction(new AlignmentAction(getParentPart(),
+				PositionConstants.BOTTOM));
+		registerAction(new AlignmentAction(getParentPart(),
+				PositionConstants.CENTER));
+		registerAction(new AlignmentAction(getParentPart(),
+				PositionConstants.MIDDLE));
+		registerAction(new MatchWidthAction(getParentPart()));
+		registerAction(new MatchHeightAction(getParentPart()));
+		IAction showGrid = new ToggleGridAction(getDiagramContainer()
+				.getGraphicalViewer());
+		getDiagramContainer().getActionRegistry().registerAction(showGrid);
+
+		// Bug 323351: Add button to toggle a flag if the context pad buttons
+		// shall be shown or not
+		IAction toggleContextButtonPad = new ToggleContextButtonPadAction(this);
+		toggleContextButtonPad.setChecked(false);
+		actionRegistry.registerAction(toggleContextButtonPad);
+		// End bug 323351
+
+		IHandlerService hs = (IHandlerService) getParentPart().getSite()
+				.getService(IHandlerService.class);
+		hs.activateHandler(FeatureExecutionHandler.COMMAND_ID,
+				new FeatureExecutionHandler(getConfigurationProvider()));
 	}
 }
