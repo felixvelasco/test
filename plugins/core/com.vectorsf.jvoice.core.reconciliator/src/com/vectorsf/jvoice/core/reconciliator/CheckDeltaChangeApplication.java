9f800d0e3a1cb7fc0d4f487cc2c6861c6c1546a5
Luis Porras <luis.escudero@servexternos.isban.es>
2013-11-19 17:36:01 +0100

    Si se cierra el proyecto desaparece de la vista del navegador y al abrirlo aparece de nuevo

--- plugins/core/com.vectorsf.jvoice.core.reconciliator/src/com/vectorsf/jvoice/core/reconciliator/CheckDeltaChangeApplication.java
+++ plugins/core/com.vectorsf.jvoice.core.reconciliator/src/com/vectorsf/jvoice/core/reconciliator/CheckDeltaChangeApplication.java
@@ -1,10 +1,12 @@
 package com.vectorsf.jvoice.core.reconciliator;
 
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.resources.IResourceDeltaVisitor;
 import org.eclipse.core.runtime.CoreException;
 
+import com.vectorsf.jvoice.core.project.JVoiceApplicationNature;
 import com.vectorsf.jvoice.model.base.JVApplication;
 
 public class CheckDeltaChangeApplication implements IResourceDeltaVisitor {
@@ -21,7 +23,14 @@ public class CheckDeltaChangeApplication implements IResourceDeltaVisitor {
 		if (delta.getKind() == IResourceDelta.REMOVED) {
 			jvProject.getModel().getProjects().remove(jvProject);
 		}
-		
+		if (delta.getKind() == IResourceDelta.CHANGED
+				&& ((delta.getFlags() & IResourceDelta.DESCRIPTION) != 0 || (delta.getFlags() & IResourceDelta.OPEN) != 0)) {
+			IProject project = (IProject) resource;
+			if (!project.isOpen() || !project.hasNature(JVoiceApplicationNature.NATURE_ID)) {
+				jvProject.getModel().getProjects().remove(jvProject);
+				return false;
+			}
+		}
 		return false;
 	}
 
