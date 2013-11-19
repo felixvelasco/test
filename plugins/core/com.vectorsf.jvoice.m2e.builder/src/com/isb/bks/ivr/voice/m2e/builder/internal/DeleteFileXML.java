99fd7ec3c2429dda7171b820bbbf4b7d5d21a92b
Esther Mateos Dominguez <esther.mateos@servexternos.isban.es>
2013-11-19 12:04:30 +0100

    Borrado XML generado cuando se borra un flow del WS

---
+++ plugins/core/com.vectorsf.jvoice.m2e.builder/src/com/isb/bks/ivr/voice/m2e/builder/internal/DeleteFileXML.java
@@ -0,0 +1,87 @@
+package com.isb.bks.ivr.voice.m2e.builder.internal;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.IResourceDeltaVisitor;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+
+import com.vectorsf.jvoice.base.model.service.BaseModel;
+
+public class DeleteFileXML implements IResourceDeltaVisitor {
+
+	private String target = "target/jVoice";
+	private IFolder folder;
+	private final static IPath pkgPath = new Path(BaseModel.JV_PATH);
+
+	public DeleteFileXML(IFolder packageFolder) {
+		folder = packageFolder;
+	}
+
+	@Override
+	public boolean visit(IResourceDelta delta) throws CoreException {
+		IResource resource = delta.getResource();
+		if (isValidFolder(resource)) {
+			if (resource.getType() != IResource.FILE) {
+				return true;
+			}
+			if (delta.getKind() == IResourceDelta.REMOVED) {
+				IFile file = (IFile) resource;
+				IPath ruta = file.getParent().getProjectRelativePath()
+						.makeRelativeTo(pkgPath);
+				String name = file.getName().replace(".jvflow", "").trim();
+				IPath pkgPathTarget = new Path(target + "/" + ruta);
+				IFolder folderTarget = file.getProject().getFolder(
+						pkgPathTarget);
+
+				for (IResource member : folderTarget.members()) {
+					if (member instanceof IFile) {
+						String nameMember = member.getName()
+								.replace(".xml", "").trim();
+						if (nameMember.equals(name)) {
+							member.delete(false, null);
+							deleteFolder(folderTarget);
+						}
+					}
+
+				}
+
+			}
+		}
+
+		return true;
+	}
+
+	private void deleteFolder(IFolder folderTarget) {
+		try {
+			if (folderTarget.members().length == 0) {
+				folderTarget.delete(true, null);
+				IFolder parent = (IFolder) folderTarget.getParent();
+				String baseTarget = folderTarget.getProject().getName() + "/"
+						+ target;
+				if (parent.exists()
+						&& !parent.getFullPath().makeRelative().toString()
+								.equals(baseTarget)
+						&& parent.members().length == 0) {
+					deleteFolder(parent);
+				}
+
+			}
+		} catch (CoreException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private boolean isValidFolder(IResource resource) {
+		if (pkgPath.isPrefixOf(resource.getProjectRelativePath())
+				&& !pkgPath.equals(resource.getProjectRelativePath())) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
--- plugins/core/com.vectorsf.jvoice.m2e.builder/src/com/isb/bks/ivr/voice/m2e/builder/internal/JVoiceModuleBuildParticipant.java
+++ plugins/core/com.vectorsf.jvoice.m2e.builder/src/com/isb/bks/ivr/voice/m2e/builder/internal/JVoiceModuleBuildParticipant.java
@@ -4,34 +4,83 @@ import java.io.File;
 import java.util.Set;
 
 import org.apache.maven.plugin.MojoExecution;
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.m2e.core.MavenPlugin;
 import org.eclipse.m2e.core.embedder.IMaven;
 import org.eclipse.m2e.core.project.configurator.MojoExecutionBuildParticipant;
 import org.sonatype.plexus.build.incremental.BuildContext;
 
+import com.vectorsf.jvoice.base.model.service.BaseModel;
+import com.vectorsf.jvoice.model.base.JVBean;
+
 public class JVoiceModuleBuildParticipant extends MojoExecutionBuildParticipant {
+
+	private IPath pkgPath = new Path(BaseModel.JV_PATH);
 
 	public JVoiceModuleBuildParticipant(MojoExecution execution) {
 		super(execution, true);
 	}
 
 	@Override
-	public Set<IProject> build(int kind, IProgressMonitor monitor) throws Exception {
+	public Set<IProject> build(int kind, IProgressMonitor monitor)
+			throws Exception {
 		IMaven maven = MavenPlugin.getMaven();
 		BuildContext buildContext = getBuildContext();
+
+		IResourceDelta delta = getDelta(getMavenProjectFacade().getProject());
+		if (delta.getKind() == IResourceDelta.CHANGED) {
+			for (IResourceDelta delt : delta.getAffectedChildren()) {
+				IResource resource = delt.getResource();
+				if (resource instanceof IFolder) {
+					IFolder folder = (IFolder) resource;
+					DeleteFileXML delete = new DeleteFileXML(folder);
+					try {
+						delta.accept(delete);
+					} catch (CoreException e) {
+					}
+				}
+
+			}
+		}
 
 		// execute mojo
 		Set<IProject> result = super.build(kind, monitor);
 
 		// tell m2e builder to refresh generated files
-		File generated = maven.getMojoParameterValue(getSession().getCurrentProject(), getMojoExecution(),
-				"outputDirectory", File.class, monitor);
+		File generated = maven.getMojoParameterValue(getSession()
+				.getCurrentProject(), getMojoExecution(), "outputDirectory",
+				File.class, monitor);
 		if (generated != null) {
 			buildContext.refresh(generated);
 		}
 
 		return result;
+	}
+
+	private IPath getRelativePath(IResource resource) {
+		if (pkgPath.isPrefixOf(resource.getProjectRelativePath())
+				&& !pkgPath.equals(resource.getProjectRelativePath())) {
+			return resource.getProjectRelativePath().makeRelativeTo(pkgPath);
+		} else {
+			return null;
+		}
+	}
+
+	private JVBean getBeanFromResource(Resource eResource) {
+		for (EObject e : eResource.getContents()) {
+			if (e instanceof JVBean) {
+				return (JVBean) e;
+			}
+		}
+		return null;
 	}
 }
