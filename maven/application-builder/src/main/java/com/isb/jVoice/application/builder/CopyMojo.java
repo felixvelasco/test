b9dd8ab7040c74f8e968ef75ae4622edfd0e005c
Esther Mateos Dominguez <esther.mateos@servexternos.isban.es>
2013-11-22 12:17:09 +0100

    Generated audios folder with the each  project name

--- maven/application-builder/src/main/java/com/isb/jVoice/application/builder/CopyMojo.java
+++ maven/application-builder/src/main/java/com/isb/jVoice/application/builder/CopyMojo.java
@@ -7,6 +7,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Arrays;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
@@ -15,6 +16,16 @@ import java.util.zip.ZipInputStream;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+
+import com.vectorsf.jvoice.model.base.BasePackage;
+import com.vectorsf.jvoice.model.base.JVModule;
+
 
 /**
  * Goal which touches a timestamp file.
@@ -33,6 +44,13 @@ public class CopyMojo extends AbstractMojo {
 	private static final String DESTINO = "flows";
 	private static final String APPSERVLET = "spring/appServlet";
 	private static final String FLOW = "-flow";
+	private static final String AUDIOS = "audios";
+	private static final String DESTINOWAV ="resources";
+	private static final String WAV = "wav";
+	private static final String XML = "xml";
+	private File file;
+	private static final String ARCHIVE_FILE=  "archive:file:/";
+	private static final String SEPARATOR2 = "\\";
 
 	/**
 	 * Location of the target directory.
@@ -54,20 +72,44 @@ public class CopyMojo extends AbstractMojo {
 	 */
 	private List<String> runtimeClasspathElements;
 
-	private void findFullPath(String fileNameToSearch, File file) throws IOException {
+	private void findFullPath(List<String> fileNameToSearch, File file) throws IOException {
 		ZipInputStream zip = new ZipInputStream(new FileInputStream(file));
 		ZipEntry ze = null;
 		String ret = null;
+		String nameProject = null;
 		while ((ze = zip.getNextEntry()) != null) {
 			String entryName = ze.getName();
+			if(entryName.equals(".projectInformation")){
+				nameProject = getNameProject(entryName, file);
+			}
+			
 			int index = entryName.lastIndexOf(DOT);
+			
 			if (index != -1) {
 				String entryNameDots = entryName.replace(SEPARATOR, DOT);
-				if (entryNameDots.endsWith(fileNameToSearch) && entryNameDots.contains(JVOICES + DOT)) {
-					ret = entryName.substring(entryName.indexOf(SEPARATOR) + 1, entryName.length());
+				/*Creamos un boolean inicializado a false y Recorremos la lista para comprobar la terminacion del fichero.
+				  En el caso de que coincida la terminacion, el booleano se pondra a true, indicando que es un tipo de fichero
+				  que necesitamos copiar del jar.
+				*/
+				boolean terminacion = false;
+				for (String element : fileNameToSearch) {
+					if(entryNameDots.endsWith(element)){
+						terminacion= true;
+						break;
+					}					
+				}							
+				// Comprobamos si el archivo es xml o jVoices para proceder a su copia. El resto no se copia en la aplicacion.
+				if (terminacion && (entryNameDots.contains(JVOICES + DOT) || entryNameDots.contains(AUDIOS + DOT))) {
 					ZipFile zipFile = new ZipFile(file);
 					InputStream inputStream = zipFile.getInputStream(ze);
-					copyFile(inputStream, ret);
+					if(entryNameDots.endsWith(DOT+WAV)){
+						ret = entryName;
+						copyFile(inputStream, ret, WAV, nameProject );
+					}else if (entryNameDots.endsWith(DOT+XML)){
+						ret = entryName.substring(entryName.indexOf(SEPARATOR) + 1, entryName.length());
+						copyFile(inputStream, ret, JVOICES, null);
+					}
+
 					zipFile.close();
 				}
 			}
@@ -77,13 +119,17 @@ public class CopyMojo extends AbstractMojo {
 
 	@Override
 	public void execute() throws MojoExecutionException {
-
+		
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("projectInformation", new XMIResourceFactoryImpl());
+		EPackage.Registry.INSTANCE.put(BasePackage.eNS_URI, BasePackage.eINSTANCE);
+		
 		for (String element : runtimeClasspathElements) {
 
 			if (new File(element).toURI().toString().endsWith(".jar")) {
-				File file = new File(element);
+				file = new File(element);
+				List<String> extensions = Arrays.asList(new String[]{".xml", ".wav"});
 				try {
-					findFullPath(".xml", file);
+					findFullPath(extensions, file);
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -112,7 +158,6 @@ public class CopyMojo extends AbstractMojo {
 		getDir(views);
 		generateFile(views, "renderHTML.jsp");
 		generateFile(views, "renderVXI.jsp");
-
 		
 	}
 
@@ -170,16 +215,31 @@ public class CopyMojo extends AbstractMojo {
 	 * @param name
 	 * 
 	 */
-	protected void copyFile(InputStream in, String name) {
+	protected void copyFile(InputStream in, String name, String type, String nameProject) {
 		File ruta = new File(name);
-		File pathname = new File(outputDirectory, DESTINO + SEPARATOR + name.replace(ruta.getName(), "").trim() + 
-				 ruta.getName().substring(0,ruta.getName().indexOf(DOT)).trim());
-		/*
-		 * Comprobamos que exista el directorio base donde vamos a crear los XMLSi no existe, se crea.
-		 */
-		getDir(pathname);
+				
+		File pathname = null;
+		File destino = null;
+		//Se crea en rutas diferentes dependiendo de si se trata de un audio type= wav o un flujo type=jVoice
+		if(type.equals(JVOICES)){			
+			pathname = new File(outputDirectory, DESTINO + SEPARATOR + name.replace(ruta.getName(), "").trim() + 
+					 ruta.getName().substring(0,ruta.getName().indexOf(DOT)).trim());
+			/*
+			 * Comprobamos que exista el directorio base donde vamos a crear los XML. Si no existe, se crea.
+			 */
+			getDir(pathname);
 
-		File destino = new File(pathname, ruta.getName().substring(0, ruta.getName().indexOf(DOT)) + FLOW + ruta.getName().substring(ruta.getName().indexOf(DOT)));
+			destino = new File(pathname, ruta.getName().substring(0, ruta.getName().indexOf(DOT)) + FLOW + ruta.getName().substring(ruta.getName().indexOf(DOT)));
+			
+		}else if(type.equals(WAV)){
+			pathname = new File(outputDirectory.getParentFile(), DESTINOWAV + SEPARATOR + name.replace(ruta.getName(), "").trim()+SEPARATOR+ nameProject);
+			/*
+			 * Comprobamos que exista el directorio base donde vamos a crear los wav. Si no existe, se crea.
+			 */
+			getDir(pathname);
+
+			destino = new File(pathname, ruta.getName());
+		}
 
 		try {
 			OutputStream out = new FileOutputStream(destino);
@@ -198,5 +258,21 @@ public class CopyMojo extends AbstractMojo {
 			e.printStackTrace();
 		}
 	}
+	
 
+	/**
+	 * @param entryName
+	 */
+	protected String getNameProject(String entryName, File file) {		
+		String name =null;
+		String ruta = ARCHIVE_FILE+file.getAbsolutePath()
+				.replace(SEPARATOR2, SEPARATOR)+ "!/"
+				+ entryName;
+		ResourceSet resSet = new ResourceSetImpl();
+		URI uri=URI.createURI(ruta);
+		Resource res = resSet.getResource(uri, true);
+		name = ((JVModule)res.getContents().get(0)).getName();
+		return name;
+	}
+	
 }
