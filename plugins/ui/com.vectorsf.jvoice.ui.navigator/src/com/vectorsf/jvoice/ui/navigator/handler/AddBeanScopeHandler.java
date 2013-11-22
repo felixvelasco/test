7a8106912adc3f26a768e5eff968bfac0fb0f2b5
Felix Velasco <fvelascov@ext.vectorsf.com>
2013-11-22 09:10:41 +0100

    Support a single components package per module

--- plugins/ui/com.vectorsf.jvoice.ui.navigator/src/com/vectorsf/jvoice/ui/navigator/handler/AddBeanScopeHandler.java
+++ plugins/ui/com.vectorsf.jvoice.ui.navigator/src/com/vectorsf/jvoice/ui/navigator/handler/AddBeanScopeHandler.java
@@ -1,5 +1,7 @@
 package com.vectorsf.jvoice.ui.navigator.handler;
 
+import java.io.IOException;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -9,13 +11,22 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.window.Window;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.handlers.HandlerUtil;
 
+import com.vectorsf.jvoice.core.uri.VegaXMLURIHandlerImpl;
+import com.vectorsf.jvoice.model.operations.ComponentBean;
 import com.vectorsf.jvoice.model.operations.Flow;
 import com.vectorsf.jvoice.model.operations.provider.flow.ScopeItemProvider;
 
@@ -30,28 +41,52 @@ public class AddBeanScopeHandler extends AbstractHandler implements IHandler {
 		}
 
 		IProject project = getProject(flow);
-		IJavaProject jProject = JavaCore.create(project);
-
 		Shell shell = HandlerUtil.getActiveShell(event);
 
-		BeanScopeAddDialog dialog = new BeanScopeAddDialog(shell, jProject);
-		dialog.open();
+		BeanScopeAddDialog dialog = new BeanScopeAddDialog(shell, (IPackageFragment) findPackage(flow, project));
+		if (dialog.open() == Window.OK) {
+			try {
+				addScopedBean(flow, dialog.getComponentBean());
+			} catch (IOException e) {
+				throw new ExecutionException("Error saving " + flow.getName(), e);
+			}
+		}
 
 		return null;
+	}
+
+	private void addScopedBean(Flow flow, ComponentBean bean) throws IOException {
+		URI uri = EcoreUtil.getURI(flow);
+		ResourceSet resourceSet = new ResourceSetImpl();
+		VegaXMLURIHandlerImpl vegaURIHandler = new VegaXMLURIHandlerImpl();
+		resourceSet.getLoadOptions().put(XMLResource.OPTION_URI_HANDLER, vegaURIHandler);
+		Flow persistedFlow = (Flow) resourceSet.getEObject(uri, true);
+
+		persistedFlow.getBeans().add(bean);
+		persistedFlow.eResource().save(null);
+	}
+
+	@SuppressWarnings("restriction")
+	private IJavaElement findPackage(Flow flow, IProject project) {
+		org.eclipse.jdt.internal.core.JavaProject javaProject = (org.eclipse.jdt.internal.core.JavaProject) JavaCore
+				.create(project);
+		try {
+			return javaProject.findPackageFragment(flow.getOwnerPackage().getOwnerModule().getComponentsPackage());
+		} catch (JavaModelException e) {
+			return null;
+		}
 	}
 
 	private IProject getProject(Flow flow) {
 		URI uri = flow.eResource().getURI();
 		IPath path = new Path(uri.toPlatformString(true));
-		return ResourcesPlugin.getWorkspace().getRoot().getFile(path)
-				.getProject();
+		return ResourcesPlugin.getWorkspace().getRoot().getFile(path).getProject();
 	}
 
 	private Flow getFlow(ExecutionEvent event) {
 		ISelection currentSelection = HandlerUtil.getCurrentSelection(event);
 		if (currentSelection instanceof IStructuredSelection) {
-			Object firstElement = ((IStructuredSelection) currentSelection)
-					.getFirstElement();
+			Object firstElement = ((IStructuredSelection) currentSelection).getFirstElement();
 			if (firstElement instanceof ScopeItemProvider) {
 				return ((ScopeItemProvider) firstElement).getFlow();
 			}
@@ -59,4 +94,5 @@ public class AddBeanScopeHandler extends AbstractHandler implements IHandler {
 
 		return null;
 	}
+
 }
--- plugins/ui/com.vectorsf.jvoice.ui.navigator/src/com/vectorsf/jvoice/ui/navigator/handler/BeanScopeAddDialog.java
+++ plugins/ui/com.vectorsf.jvoice.ui.navigator/src/com/vectorsf/jvoice/ui/navigator/handler/BeanScopeAddDialog.java
@@ -3,9 +3,8 @@ package com.vectorsf.jvoice.ui.navigator.handler;
 import org.eclipse.jdt.core.IAnnotation;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IImportDeclaration;
-import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IMemberValuePair;
 import org.eclipse.jdt.core.IPackageFragment;
-import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.ITypeRoot;
 import org.eclipse.jdt.core.JavaModelException;
@@ -16,34 +15,39 @@ import org.eclipse.jface.dialogs.TitleAreaDialog;
 import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerFilter;
+import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
+import com.vectorsf.jvoice.model.operations.ComponentBean;
+import com.vectorsf.jvoice.model.operations.OperationsFactory;
+
 public class BeanScopeAddDialog extends TitleAreaDialog {
 
-	private Text txtName;
-	private Text labelNameText;
-	private Text valueNameText;
-	private IJavaProject jProject;
+	private Text scopedNameText;
+	private Text beanClassText;
+	private Text beanNameText;
+	private IPackageFragment packageFragment;
 	private Shell shell;
 
-	public BeanScopeAddDialog(Shell parentShell) {
-		super(parentShell);
-	}
+	private ComponentBean bean;
 
-	public BeanScopeAddDialog(Shell parentShell, IJavaProject jProject) {
+	public BeanScopeAddDialog(Shell parentShell, IPackageFragment packageFragment) {
 		super(parentShell);
-		this.jProject = jProject;
+		this.packageFragment = packageFragment;
 		this.shell = parentShell;
+		this.bean = OperationsFactory.eINSTANCE.createComponentBean();
 	}
 
 	@Override
@@ -77,10 +81,17 @@ public class BeanScopeAddDialog extends TitleAreaDialog {
 		dataName.grabExcessHorizontalSpace = true;
 		dataName.horizontalAlignment = GridData.FILL;
 		// dataName.horizontalSpan = 2;
-		txtName = new Text(container, SWT.BORDER);
-		txtName.setLayoutData(dataName);
+		scopedNameText = new Text(container, SWT.BORDER);
+		scopedNameText.setLayoutData(dataName);
+		scopedNameText.addModifyListener(new ModifyListener() {
 
-		Label lbtSpace = new Label(container, SWT.NONE);
+			@Override
+			public void modifyText(ModifyEvent e) {
+				updateComponent(scopedNameText.getText());
+			}
+		});
+
+		new Label(container, SWT.NONE);
 	}
 
 	private void createLabelName(Composite container) {
@@ -90,28 +101,35 @@ public class BeanScopeAddDialog extends TitleAreaDialog {
 		GridData dataJavaName = new GridData();
 		dataJavaName.grabExcessHorizontalSpace = true;
 		dataJavaName.horizontalAlignment = GridData.FILL;
-		labelNameText = new Text(container, SWT.BORDER);
-		labelNameText.setLayoutData(dataJavaName);
-		labelNameText.setEditable(false);
+		beanClassText = new Text(container, SWT.BORDER);
+		beanClassText.setLayoutData(dataJavaName);
+		beanClassText.setEditable(false);
 
 		// browse button on right
 		Button browse = new Button(container, SWT.PUSH);
 		browse.setText("...");
-		browse.addListener(SWT.Selection, new Listener() {
+		browse.addSelectionListener(new SelectionListener() {
 
 			@Override
-			public void handleEvent(Event event) {
-				StandardJavaElementContentProvider contentProvider = new StandardJavaElementContentProvider(
-						false);
-				ILabelProvider labelProvider = new JavaElementLabelProvider(
-						JavaElementLabelProvider.SHOW_BASICS);
+			public void widgetSelected(SelectionEvent e) {
+				StandardJavaElementContentProvider contentProvider = new StandardJavaElementContentProvider(false);
+				ILabelProvider labelProvider = new JavaElementLabelProvider(JavaElementLabelProvider.SHOW_BASICS);
 
-				ComponentsSelectionDialog dialog = new ComponentsSelectionDialog(
-						shell, labelProvider, contentProvider);
-				dialog.setInput(jProject);
+				ComponentsSelectionDialog dialog = new ComponentsSelectionDialog(shell, labelProvider, contentProvider);
+				dialog.setInput(packageFragment);
 				dialog.addFilter(new ComponentFilter());
-				dialog.open();
+				if (dialog.open() == Window.OK) {
+					ITypeRoot typeRoot = (ITypeRoot) dialog.getFirstResult();
+					updateComponent(typeRoot);
+					beanClassText.setText(bean.getFqdn());
+					beanNameText.setText(bean.getNameBean());
+				}
 			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+			}
+
 		});
 	}
 
@@ -122,10 +140,10 @@ public class BeanScopeAddDialog extends TitleAreaDialog {
 		GridData dataValueName = new GridData();
 		dataValueName.grabExcessHorizontalSpace = true;
 		dataValueName.horizontalAlignment = GridData.FILL;
-		valueNameText = new Text(container, SWT.BORDER);
-		valueNameText.setLayoutData(dataValueName);
-		valueNameText.setEditable(false);
-		valueNameText.setEnabled(false);
+		beanNameText = new Text(container, SWT.BORDER);
+		beanNameText.setLayoutData(dataValueName);
+		beanNameText.setEditable(false);
+		beanNameText.setEnabled(false);
 	}
 
 	@Override
@@ -133,69 +151,86 @@ public class BeanScopeAddDialog extends TitleAreaDialog {
 		return true;
 	}
 
-	@Override
-	protected void okPressed() {
-		// saveInput();
-		super.okPressed();
+	private void updateComponent(ITypeRoot typeRoot) {
+		IType type = typeRoot.findPrimaryType();
+		String name = firstToLowercase(type.getElementName());
+
+		IAnnotation annotation = findAnnotation(typeRoot);
+		try {
+			IMemberValuePair[] memberValuePairs = annotation.getMemberValuePairs();
+			if (memberValuePairs.length > 0) {
+				name = (String) memberValuePairs[0].getValue();
+			}
+		} catch (JavaModelException e) {
+		}
+
+		bean.setFqdn(type.getFullyQualifiedName());
+		bean.setNameBean(name);
+	}
+
+	private String firstToLowercase(String elementName) {
+		if (elementName.isEmpty()) {
+			return elementName;
+		}
+
+		char c = elementName.charAt(0);
+		if (Character.isUpperCase(c)) {
+			return Character.toLowerCase(c) + elementName.substring(1);
+		} else {
+			return elementName;
+		}
+	}
+
+	private void updateComponent(String componentName) {
+		bean.setName(componentName);
+	}
+
+	private IAnnotation findAnnotation(ITypeRoot unit) {
+		IType type = unit.findPrimaryType();
+
+		if (type == null) {
+			return null;
+		}
+
+		try {
+			for (IAnnotation annotation : type.getAnnotations()) {
+				String elementName = annotation.getElementName();
+				if (elementName.equals("org.springframework.stereotype.Component")) {
+					return annotation;
+				} else if (elementName.equals("Component") && unit instanceof ICompilationUnit) {
+					for (IImportDeclaration _import : ((ICompilationUnit) unit).getImports()) {
+						String importedType = _import.getElementName();
+						if (importedType.equals("org.springframework.stereotype.Component")) {
+							return annotation;
+						}
+						if (importedType.equals("org.springframework.stereotype.*")) {
+							return annotation;
+						}
+					}
+				}
+			}
+		} catch (JavaModelException e) {
+			return null;
+		}
+
+		return null;
 	}
 
 	public class ComponentFilter extends ViewerFilter {
 
 		@Override
-		public boolean select(Viewer viewer, Object parentElement,
-				Object element) {
+		public boolean select(Viewer viewer, Object parentElement, Object element) {
 			if (element instanceof ITypeRoot) {
-				ITypeRoot unit = (ITypeRoot) element;
-				IType type = unit.findPrimaryType();
 
-				if (type == null) {
-					return false;
-				}
+				return findAnnotation((ITypeRoot) element) != null;
 
-				IAnnotation[] annotations;
-				try {
-					annotations = type.getAnnotations();
-					for (IAnnotation annotation : annotations) {
-						String elementName = annotation.getElementName();
-						if (elementName
-								.equals("org.springframework.stereotype.Component")) {
-							return true;
-						} else if (elementName.equals("Component")
-								&& element instanceof ICompilationUnit) {
-							for (IImportDeclaration _import : ((ICompilationUnit) unit)
-									.getImports()) {
-								String importedType = _import.getElementName();
-								if (importedType
-										.equals("org.springframework.stereotype.Component")) {
-									return true;
-								}
-								if (importedType
-										.equals("org.springframework.stereotype.*")) {
-									return true;
-								}
-							}
-						}
-					}
-				} catch (JavaModelException e) {
-					return false;
-				}
-
-				return false;
-			}
-
-			if (element instanceof IPackageFragment) {
-				try {
-					return ((IPackageFragment) element).hasChildren();
-				} catch (JavaModelException e) {
-					return false;
-				}
-			}
-			if (element instanceof IPackageFragmentRoot) {
-				// return !((IPackageFragmentRoot) element).isArchive();
-				return true;
 			}
 			return false;
 		}
+	}
+
+	public ComponentBean getComponentBean() {
+		return bean;
 	}
 
 }
