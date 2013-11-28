777cf12bb6c79085db3b62c06aee2b6611ad8f1e
pruizmag <paloma.ruiz@servexternos.isban.es>
2013-11-28 15:32:34 +0100

    Include parameters bean and method in call state

--- plugins/ui/com.vectorsf.jvoice.ui.diagram.properties/src/com/vectorsf/jvoice/ui/diagram/properties/filters/CallStateSection.java
+++ plugins/ui/com.vectorsf.jvoice.ui.diagram.properties/src/com/vectorsf/jvoice/ui/diagram/properties/filters/CallStateSection.java
@@ -8,13 +8,37 @@ import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
 
 import com.vectorsf.jvoice.model.operations.CallState;
 import com.vectorsf.jvoice.model.operations.ParameterizedState;
 
 public class CallStateSection extends ParametrizableStateSection {
 
+	private LabelAndText bean;
+	private LabelAndText method;
+
 	public CallStateSection() {
+	}
+
+	@Override
+	public void createControls(Composite parent, TabbedPropertySheetPage atabbedPropertySheetPage) {
+		super.createControls(parent, atabbedPropertySheetPage);
+
+		bean = createLabelAndText("Bean", "", null, -1);
+		bean.text.setEnabled(false);
+
+		method = createLabelAndText("Method", "", null, -1);
+		method.text.setEnabled(false);
+	}
+
+	@Override
+	public void refresh() {
+		super.refresh();
+
+		bean.text.setText(((CallState) state).getBean().getNameBean());
+		method.text.setText(((CallState) state).getMethodName());
 	}
 
 	@Override
