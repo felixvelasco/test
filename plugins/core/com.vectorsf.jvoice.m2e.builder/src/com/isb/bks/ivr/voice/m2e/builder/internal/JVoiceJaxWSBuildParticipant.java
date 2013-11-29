62353872546358c972f386c5c9b6eb0ff1fe5957
pruizmag <paloma.ruiz@servexternos.isban.es>
2013-11-29 15:55:44 +0100

    Add JaxWS

---
+++ plugins/core/com.vectorsf.jvoice.m2e.builder/src/com/isb/bks/ivr/voice/m2e/builder/internal/JVoiceJaxWSBuildParticipant.java
@@ -0,0 +1,40 @@
+package com.isb.bks.ivr.voice.m2e.builder.internal;
+
+import java.io.File;
+import java.util.Set;
+
+import org.apache.maven.plugin.MojoExecution;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.m2e.core.MavenPlugin;
+import org.eclipse.m2e.core.embedder.IMaven;
+import org.eclipse.m2e.core.project.configurator.MojoExecutionBuildParticipant;
+import org.sonatype.plexus.build.incremental.BuildContext;
+
+public class JVoiceJaxWSBuildParticipant extends MojoExecutionBuildParticipant {
+
+	public JVoiceJaxWSBuildParticipant(MojoExecution execution) {
+		super(execution, true);
+	}
+
+	@Override
+	public Set<IProject> build(int kind, IProgressMonitor monitor)
+			throws Exception {
+
+		IMaven maven = MavenPlugin.getMaven();
+		BuildContext buildContext = getBuildContext();
+
+		// execute mojo
+		Set<IProject> result = super.build(kind, monitor);
+
+		// tell m2e builder to refresh generated files
+		File generated = maven.getMojoParameterValue(getSession()
+				.getCurrentProject(), getMojoExecution(), "sourceDestDir",
+				File.class, monitor);
+		if (generated != null) {
+			buildContext.refresh(generated);
+		}
+
+		return result;
+	}
+}
---
+++ plugins/core/com.vectorsf.jvoice.m2e.builder/src/com/isb/bks/ivr/voice/m2e/builder/internal/JVoiceJaxWSConfigurator.java
@@ -0,0 +1,29 @@
+package com.isb.bks.ivr.voice.m2e.builder.internal;
+
+import org.apache.maven.plugin.MojoExecution;
+import org.eclipse.m2e.core.lifecyclemapping.model.IPluginExecutionMetadata;
+import org.eclipse.m2e.core.project.IMavenProjectFacade;
+import org.eclipse.m2e.core.project.configurator.AbstractBuildParticipant;
+import org.eclipse.m2e.jdt.AbstractSourcesGenerationProjectConfigurator;
+
+public class JVoiceJaxWSConfigurator extends
+
+AbstractSourcesGenerationProjectConfigurator {
+
+	@Override
+	public AbstractBuildParticipant getBuildParticipant(
+
+	IMavenProjectFacade projectFacade, MojoExecution execution,
+
+	IPluginExecutionMetadata executionMetadata) {
+
+		return new JVoiceJaxWSBuildParticipant(execution);
+
+	}
+
+	@Override
+	protected String getOutputFolderParameterName() {
+		return "sourceDestDir";
+	}
+
+}
