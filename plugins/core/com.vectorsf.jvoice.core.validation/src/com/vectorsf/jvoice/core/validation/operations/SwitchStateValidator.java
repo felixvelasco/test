1397ab4911204664827575b7dc304568d0a91d95
sergiomilla <sergio.milla@servexternos.isban.es>
2013-12-18 17:13:54 +0100

    Added distintEventNamesSwitch validation

--- plugins/core/com.vectorsf.jvoice.core.validation/src/com/vectorsf/jvoice/core/validation/operations/SwitchStateValidator.java
+++ plugins/core/com.vectorsf.jvoice.core.validation/src/com/vectorsf/jvoice/core/validation/operations/SwitchStateValidator.java
@@ -50,4 +50,21 @@ public class SwitchStateValidator {
 		}
 		return true;
 	}
+
+	public boolean validate_SwitchState_distintEventNamesSwitch(SwitchState state) {
+		if (state.getCase().size() > 1) {
+			for (int i = 0; i < state.getCase().size() - 1; i++) {
+				Case case1 = state.getCase().get(i);
+				for (int j = i + 1; j < state.getCase().size(); j++) {
+					Case case2 = state.getCase().get(j);
+
+					if (case2.getEventName().equals(case2.getEventName())) {
+						operationsValidator.error(state, "Duplicate case \"" + case1.getEventName() + "\" in switch "
+								+ state.getName());
+					}
+				}
+			}
+		}
+		return true;
+	}
 }
