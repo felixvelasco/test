7f2d7952718ffd0b574b8207344fe9675315048d
Luis Porras <luis.escudero@servexternos.isban.es>
2013-12-19 16:00:24 +0100

    verifica que el subflow exista

--- plugins/core/com.vectorsf.jvoice.core.validation/src/com/vectorsf/jvoice/core/validation/operations/CallFlowStateValidator.java
+++ plugins/core/com.vectorsf.jvoice.core.validation/src/com/vectorsf/jvoice/core/validation/operations/CallFlowStateValidator.java
@@ -1,7 +1,12 @@
 package com.vectorsf.jvoice.core.validation.operations;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.emf.common.util.URI;
 
 import com.vectorsf.jvoice.model.operations.CallFlowState;
 import com.vectorsf.jvoice.model.operations.FinalState;
@@ -13,6 +18,30 @@ public class CallFlowStateValidator {
 
 	public CallFlowStateValidator(OperationsValidator operationsValidator) {
 		this.operationsValidator = operationsValidator;
+	}
+
+	public boolean validate_CallFlowState_checkSubflowState(CallFlowState state) {
+
+		if (state.getSubflow() == null) {
+			operationsValidator.error(state, "State " + state.getName() + " has no subflow defined.");
+		} else {
+			URI uri = state.getSubflow().eResource().getURI();
+			File rawFile = null;
+			if (uri.isPlatformResource()) {
+				IPath rawPath = ResourcesPlugin.getWorkspace().getRoot().findMember(uri.toPlatformString(true))
+						.getRawLocation();
+
+				rawFile = rawPath.toFile();
+			} else {
+				rawFile = new File(uri.toFileString());
+			}
+			if (!rawFile.exists()) {
+				operationsValidator
+						.error(state, "State " + state.getName() + " has a subflow defined that not exists.");
+			}
+		}
+
+		return true;
 	}
 
 	public boolean validate_CallFlowState_outcomingSubflowState(CallFlowState state) {
