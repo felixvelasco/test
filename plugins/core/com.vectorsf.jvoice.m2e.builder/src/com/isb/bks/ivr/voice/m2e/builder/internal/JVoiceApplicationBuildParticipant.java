7801fd9121721533a06dd9e988bd3d2c6c73f280
idafereyes <idafe.reyes@servexternos.isban.es>
2014-01-13 18:09:10 +0100

    Resources directories refreshing when building app.

--- plugins/core/com.vectorsf.jvoice.m2e.builder/src/com/isb/bks/ivr/voice/m2e/builder/internal/JVoiceApplicationBuildParticipant.java
+++ plugins/core/com.vectorsf.jvoice.m2e.builder/src/com/isb/bks/ivr/voice/m2e/builder/internal/JVoiceApplicationBuildParticipant.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.util.Set;
 
 import org.apache.maven.plugin.MojoExecution;
-import org.codehaus.plexus.util.Scanner;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.m2e.core.MavenPlugin;
@@ -12,35 +11,46 @@ import org.eclipse.m2e.core.embedder.IMaven;
 import org.eclipse.m2e.core.project.configurator.MojoExecutionBuildParticipant;
 import org.sonatype.plexus.build.incremental.BuildContext;
 
-public class JVoiceApplicationBuildParticipant extends MojoExecutionBuildParticipant {
+public class JVoiceApplicationBuildParticipant extends
+		MojoExecutionBuildParticipant {
 
 	public JVoiceApplicationBuildParticipant(MojoExecution execution) {
 		super(execution, true);
 	}
 
 	@Override
-	public Set<IProject> build(int kind, IProgressMonitor monitor) throws Exception {
+	public Set<IProject> build(int kind, IProgressMonitor monitor)
+			throws Exception {
 		IMaven maven = MavenPlugin.getMaven();
 		BuildContext buildContext = getBuildContext();
-
-		// check if any of the grammar files changed
-		File source = maven.getMojoParameterValue(getSession().getCurrentProject(), getMojoExecution(),
-				"sourceDirectory", File.class, monitor);
-		Scanner ds = buildContext.newScanner(source); // delta or full scanner
-		ds.scan();
-		String[] includedFiles = ds.getIncludedFiles();
-		if (includedFiles == null || includedFiles.length <= 0) {
-			return null;
-		}
 
 		// execute mojo
 		Set<IProject> result = super.build(kind, monitor);
 
 		// tell m2e builder to refresh generated files
-		File generated = maven.getMojoParameterValue(getSession().getCurrentProject(), getMojoExecution(),
-				"outputDirectory", File.class, monitor);
+		File generated = maven.getMojoParameterValue(getSession()
+				.getCurrentProject(), getMojoExecution(), "outputDirectory",
+				File.class, monitor);
+
 		if (generated != null) {
 			buildContext.refresh(generated);
+		}
+
+		// refreshes /src/main/webapp/resources directory if exists
+		File webAppResources = new File(getSession().getCurrentProject()
+				.getBasedir() + "/src/main/webapp/resources");
+
+		if (webAppResources != null) {
+			buildContext.refresh(webAppResources);
+		}
+
+		int index = getSession().getCurrentProject().getResources().size();
+		String resourceDir;
+		// refreshes each resources directory
+		for (int i = 0; i < index; i++) {
+			resourceDir = getSession().getCurrentProject().getResources()
+					.get(i).getDirectory();
+			buildContext.refresh(new File(resourceDir));
 		}
 
 		return result;
--- plugins/core/com.vectorsf.jvoice.m2e.builder/src/com/isb/bks/ivr/voice/m2e/builder/internal/JVoiceProjectConfigurator.java
+++ plugins/core/com.vectorsf.jvoice.m2e.builder/src/com/isb/bks/ivr/voice/m2e/builder/internal/JVoiceProjectConfigurator.java
@@ -1,24 +1,30 @@
 package com.isb.bks.ivr.voice.m2e.builder.internal;
 
 import org.apache.maven.plugin.MojoExecution;
-import org.eclipse.core.runtime.*;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.m2e.core.lifecyclemapping.model.IPluginExecutionMetadata;
 import org.eclipse.m2e.core.project.IMavenProjectFacade;
-import org.eclipse.m2e.core.project.configurator.*;
+import org.eclipse.m2e.core.project.configurator.AbstractBuildParticipant;
+import org.eclipse.m2e.core.project.configurator.AbstractProjectConfigurator;
+import org.eclipse.m2e.core.project.configurator.ProjectConfigurationRequest;
 
-public class JVoiceProjectConfigurator extends AbstractProjectConfigurator
-{
+public class JVoiceProjectConfigurator extends AbstractProjectConfigurator {
 
 	@Override
-	public AbstractBuildParticipant getBuildParticipant(IMavenProjectFacade projectFacade, MojoExecution execution,
-		IPluginExecutionMetadata executionMetadata)
-	{
-		return new JVoiceModuleBuildParticipant(execution);
+	public AbstractBuildParticipant getBuildParticipant(
+			IMavenProjectFacade projectFacade, MojoExecution execution,
+			IPluginExecutionMetadata executionMetadata) {
+		if (execution.getGoal().equals("copyXML")) {
+			return new JVoiceApplicationBuildParticipant(execution);
+		} else {
+			return new JVoiceModuleBuildParticipant(execution);
+		}
 	}
 
 	@Override
-	public void configure(ProjectConfigurationRequest request, IProgressMonitor monitor) throws CoreException
-	{
+	public void configure(ProjectConfigurationRequest request,
+			IProgressMonitor monitor) throws CoreException {
 	}
 
 }
