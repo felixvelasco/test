cc8e46ae22c69d5ac3360416e36a5d96799a73e4
José Luis Gonzalo <jgonzalo99@gmail.com>
2014-01-24 10:58:56 +0100

    Creación del diálogo de eventos

---
+++ plugins/diagram/com.vectorsf.jvoice.diagram.core/src/com/vectorsf/jvoice/diagram/core/pattern/transition/EventsDialog.java
@@ -0,0 +1,155 @@
+package com.vectorsf.jvoice.diagram.core.pattern.transition;
+
+import java.util.*;
+import java.util.List;
+
+import org.eclipse.jface.dialogs.*;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.resource.StringConverter;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.*;
+import org.eclipse.swt.widgets.*;
+
+public class EventsDialog extends Dialog {
+
+	private String title;
+	private Button okButton;
+	private List<Button> buttons;
+	private Text errorMessageText;
+	private String errorMessage;
+
+	private ButtonInfo[] options;
+
+	private String[] values = new String[0];
+
+	public EventsDialog(Shell parentShell, String dialogTitle, List<ButtonInfo> options) {
+		super(parentShell);
+		this.title = dialogTitle;
+		this.options = options.toArray(new ButtonInfo[options.size()]);
+		// this.extraOptions = extraOptions.toArray(new ButtonInfo[extraOptions.size()]);
+		this.buttons = new ArrayList<Button>();
+	}
+
+	@Override
+	protected void buttonPressed(int buttonId) {
+		if (buttonId == IDialogConstants.OK_ID) {
+			values = retrieveValues();
+		} else {
+			values = new String[0];
+		}
+		super.buttonPressed(buttonId);
+	}
+
+	private String[] retrieveValues() {
+		List<String> checked = new ArrayList<String>();
+		for (Button b : buttons) {
+			if (b.getSelection()) {
+				checked.add(b.getText());
+			}
+		}
+
+		return checked.toArray(new String[checked.size()]);
+	}
+
+	@Override
+	protected void configureShell(Shell shell) {
+		super.configureShell(shell);
+		if (title != null) {
+			shell.setText(title);
+		}
+	}
+
+	@Override
+	protected void createButtonsForButtonBar(Composite parent) {
+		okButton = createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
+		createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
+	}
+
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		// create composite
+		final Composite composite = (Composite) super.createDialogArea(parent);
+		Composite buttonComposite = new Composite(composite, SWT.WRAP);
+		GridData data = new GridData(GridData.GRAB_HORIZONTAL | GridData.GRAB_VERTICAL | GridData.HORIZONTAL_ALIGN_FILL
+				| GridData.VERTICAL_ALIGN_CENTER);
+		buttonComposite.setLayoutData(data);
+		buttonComposite.setLayout(new GridLayout(1, false));
+
+		for (int i = 0; i < options.length; i++) {
+			Button b = new Button(buttonComposite, SWT.CHECK);
+			b.setImage(options[i].image);
+			b.setData(options[i].name);
+			b.setText(options[i].name);
+			b.setSelection(options[i].selected);
+			b.setEnabled(options[i].editable);
+			b.setLayoutData(data);
+			b.setToolTipText(options[i].name);
+			buttons.add(b);
+		}
+
+		// Composite textComposite = new Composite(composite, SWT.WRAP);
+		// data = new GridData(GridData.GRAB_HORIZONTAL | GridData.GRAB_VERTICAL | GridData.HORIZONTAL_ALIGN_FILL
+		// | GridData.VERTICAL_ALIGN_CENTER);
+		// textComposite.setLayoutData(data);
+		// textComposite.setLayout(new GridLayout(2, false));
+
+		// extraButtonComposite = new Composite(composite, SWT.WRAP);
+		// data = new GridData(GridData.GRAB_HORIZONTAL | GridData.GRAB_VERTICAL | GridData.HORIZONTAL_ALIGN_FILL
+		// | GridData.VERTICAL_ALIGN_CENTER);
+		// extraButtonComposite.setLayoutData(data);
+		// extraButtonComposite.setLayout(new GridLayout(3, false));
+
+		// for (int i = 0; i < extraOptions.length; i++) {
+		// addExtraButton(extraOptions[i]);
+		// }
+
+		errorMessageText = new Text(composite, SWT.READ_ONLY | SWT.WRAP);
+		errorMessageText.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL | GridData.HORIZONTAL_ALIGN_FILL));
+		errorMessageText.setBackground(errorMessageText.getDisplay().getSystemColor(SWT.COLOR_WIDGET_BACKGROUND));
+		// Set the error message text
+		// See https://bugs.eclipse.org/bugs/show_bug.cgi?id=66292
+		setErrorMessage(errorMessage);
+
+		applyDialogFont(composite);
+		return composite;
+	}
+
+	protected Button getOkButton() {
+		return okButton;
+	}
+
+	public String[] getValues() {
+		return values;
+	}
+
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+		if (errorMessageText != null && !errorMessageText.isDisposed()) {
+			errorMessageText.setText(errorMessage == null ? " \n " : errorMessage); //$NON-NLS-1$
+			boolean hasError = errorMessage != null && StringConverter.removeWhiteSpaces(errorMessage).length() > 0;
+			errorMessageText.setEnabled(hasError);
+			errorMessageText.setVisible(hasError);
+			errorMessageText.getParent().update();
+		}
+	}
+
+	protected int getInputComboStyle() {
+		return SWT.SIMPLE | SWT.BORDER;
+	}
+
+	public static final class ButtonInfo {
+		public final String name;
+		public final Image image;
+		public final boolean selected;
+		public final boolean editable;
+
+		public ButtonInfo(String name, Image image, boolean selected, boolean editable) {
+			this.name = name;
+			this.image = image;
+			this.selected = selected;
+			this.editable = editable;
+		}
+	}
+
+}
---
+++ plugins/diagram/com.vectorsf.jvoice.diagram.core/src/com/vectorsf/jvoice/diagram/core/pattern/transition/Messages.java
@@ -0,0 +1,20 @@
+package com.vectorsf.jvoice.diagram.core.pattern.transition;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "com.vectorsf.jvoice.diagram.core.pattern.transition.messages"; //$NON-NLS-1$
+	public static String AddMoreEventFeature_moreEvents;
+	public static String AddMoreEventFeature_dialogTitle;
+	public static String AddMoreEventFeature_name;
+	public static String AddMoreEventFeature_newEvent;
+	public static String AddMoreEventFeature_selectName;
+	public static String AddMoreEventFeature_addCustomEvent;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
