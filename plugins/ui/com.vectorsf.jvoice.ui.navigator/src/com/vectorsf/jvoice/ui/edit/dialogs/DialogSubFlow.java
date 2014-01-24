88d4cbb47a55f2ebdc742ae93b4db5827e9e0231
José Luis Gonzalo <jgonzalo99@gmail.com>
2014-01-24 16:47:17 +0100

    Abre el árbol

--- plugins/ui/com.vectorsf.jvoice.ui.navigator/src/com/vectorsf/jvoice/ui/edit/dialogs/DialogSubFlow.java
+++ plugins/ui/com.vectorsf.jvoice.ui.navigator/src/com/vectorsf/jvoice/ui/edit/dialogs/DialogSubFlow.java
@@ -1,62 +1,62 @@
 package com.vectorsf.jvoice.ui.edit.dialogs;
 
 import org.eclipse.jface.dialogs.IDialogConstants;
-import org.eclipse.jface.viewers.ILabelProvider;
-import org.eclipse.jface.viewers.ITreeContentProvider;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.dialogs.ElementTreeSelectionDialog;
-import org.eclipse.ui.dialogs.FilteredTree;
-import org.eclipse.ui.dialogs.PatternFilter;
+import org.eclipse.jface.viewers.*;
+import org.eclipse.swt.widgets.*;
+import org.eclipse.ui.dialogs.*;
 
 public class DialogSubFlow extends ElementTreeSelectionDialog {
 
-	private static boolean createAvailable;
+    private static boolean createAvailable;
 
-	public DialogSubFlow(Shell parent, ILabelProvider labelProvider,
-			ITreeContentProvider contentProvider) {
-		super(parent, labelProvider, contentProvider);
+    public DialogSubFlow(Shell parent, ILabelProvider labelProvider,
+	    ITreeContentProvider contentProvider) {
+	super(parent, labelProvider, contentProvider);
+    }
+
+    @Override
+    protected org.eclipse.jface.viewers.TreeViewer doCreateTreeViewer(
+	    org.eclipse.swt.widgets.Composite parent, int style) {
+	PatternFilter filter = new PatternFilter();
+	FilteredTree filteredTree = new FilteredTree(parent, style, filter,
+		true);
+	return filteredTree.getViewer();
+
+    }
+
+    @Override
+    protected void createButtonsForButtonBar(Composite parent) {
+	if (isButtonCreatevailable()) {
+	    createButton(parent, IDialogConstants.PROCEED_ID, "Create", false);
 	}
 
-	@Override
-	protected org.eclipse.jface.viewers.TreeViewer doCreateTreeViewer(
-			org.eclipse.swt.widgets.Composite parent, int style) {
-		PatternFilter filter = new PatternFilter();
-		FilteredTree filteredTree = new FilteredTree(parent, style, filter,
-				true);
-		return filteredTree.getViewer();
+	// Expandimos el árbol
+	getTreeViewer().expandAll();
 
+	super.createButtonsForButtonBar(parent);
+
+    }
+
+    public boolean isButtonCreatevailable() {
+	return createAvailable;
+    }
+
+    public void setIsButtonCreatevailable(boolean buttonAvailable) {
+	createAvailable = buttonAvailable;
+    }
+
+    @Override
+    protected void buttonPressed(int buttonId) {
+	if (IDialogConstants.PROCEED_ID == buttonId) {
+	    createPressed();
+	} else {
+	    super.buttonPressed(buttonId);
 	}
+    }
 
-	@Override
-	protected void createButtonsForButtonBar(Composite parent) {
-		if (isButtonCreatevailable()) {
-			createButton(parent, IDialogConstants.PROCEED_ID, "Create", false);
-		}
-		super.createButtonsForButtonBar(parent);
-
-	}
-
-	public boolean isButtonCreatevailable() {
-		return createAvailable;
-	}
-
-	public void setIsButtonCreatevailable(boolean buttonAvailable) {
-		createAvailable = buttonAvailable;
-	}
-
-	@Override
-	protected void buttonPressed(int buttonId) {
-		if (IDialogConstants.PROCEED_ID == buttonId) {
-			createPressed();
-		} else {
-			super.buttonPressed(buttonId);
-		}
-	}
-
-	protected void createPressed() {
-		setReturnCode(IDialogConstants.PROCEED_ID);
-		close();
-	}
+    protected void createPressed() {
+	setReturnCode(IDialogConstants.PROCEED_ID);
+	close();
+    }
 
 }
