72ffeb27551cac6e2bbc394708eab592c0172f59
José Luis Gonzalo <jgonzalo99@gmail.com>
2014-01-29 10:23:16 +0100

    Nuevos iconos

--- plugins/diagram/com.vectorsf.jvoice.diagram.core/src/com/vectorsf/jvoice/diagram/core/features/CoreImageProvider.java
+++ plugins/diagram/com.vectorsf.jvoice.diagram.core/src/com/vectorsf/jvoice/diagram/core/features/CoreImageProvider.java
@@ -2,7 +2,8 @@ package com.vectorsf.jvoice.diagram.core.features;
 
 import org.eclipse.graphiti.ui.platform.AbstractImageProvider;
 
-public class CoreImageProvider extends AbstractImageProvider {
+public class CoreImageProvider extends AbstractImageProvider
+{
 
 	// States
 	public static final String IMG_TRANSACTION_PAD = "transition_pad"; //$NON-NLS-1$
@@ -16,45 +17,76 @@ public class CoreImageProvider extends AbstractImageProvider {
 	public static final String IMG_DROPDOWN = "icon_deco_dropdown";
 
 	public static final String IMG_PALETTE_CUSTOM = "icon_est_custom";
+
 	public static final String IMG_PALETTE_EXECUTION = "icon_est_execution";
+
 	public static final String IMG_PALETTE_FINAL = "icon_est_final";
+
 	public static final String IMG_PALETTE_INPUT = "icon_est_input";
+
 	public static final String IMG_PALETTE_MENU = "icon_est_menu";
+
 	public static final String IMG_PALETTE_PROMPT = "icon_est_prompt";
+
 	public static final String IMG_PALETTE_RECORD = "icon_est_record";
+
 	public static final String IMG_PALETTE_SUBFLOW = "icon_est_subflow";
+
 	public static final String IMG_PALETTE_SWITCH = "icon_est_switch";
+
 	public static final String IMG_PALETTE_TRANSFER = "icon_est_transfer";
+
 	public static final String IMG_PALETTE_NOTE = "icon_notes";
 
 	public static final String IMG_MORE_EVENTS_DIALOG = "icon_more_events_dialog";
 
 	public static final String IMG_LARGE_PALETTE_CUSTOM = "large_icon_est_custom";
+
 	public static final String IMG_LARGE_PALETTE_EXECUTION = "large_icon_est_execution";
+
 	public static final String IMG_LARGE_PALETTE_FINAL = "large_icon_est_final";
+
 	public static final String IMG_LARGE_PALETTE_INPUT = "large_icon_est_input";
+
 	public static final String IMG_LARGE_PALETTE_MENU = "large_icon_est_menu";
+
 	public static final String IMG_LARGE_PALETTE_PROMPT = "large_icon_est_prompt";
+
 	public static final String IMG_LARGE_PALETTE_RECORD = "large_icon_est_record";
+
 	public static final String IMG_LARGE_PALETTE_SUBFLOW = "large_icon_est_subflow";
+
 	public static final String IMG_LARGE_PALETTE_SWITCH = "large_icon_est_switch";
+
 	public static final String IMG_LARGE_PALETTE_TRANSFER = "large_icon_est_transfer";
+
 	public static final String IMG_LARGE_PALETTE_NOTE = "large_icon_notes";
 
 	public static final String IMG_STATE_CUSTOM = "icon_comp_custom";
+
 	public static final String IMG_STATE_EXECUTION = "icon_comp_execution";
+
 	public static final String IMG_STATE_FINAL = "icon_comp_final";
+
 	public static final String IMG_STATE_HANGUP = "icon_comp_hangup";
+
 	public static final String IMG_STATE_INPUT = "icon_comp_input";
+
 	public static final String IMG_STATE_MENU = "icon_comp_menu";
+
 	public static final String IMG_STATE_PROMPT = "icon_comp_prompt";
+
 	public static final String IMG_STATE_RECORD = "icon_comp_record";
+
 	public static final String IMG_STATE_SUBFLOW = "icon_comp_subflow";
+
 	public static final String IMG_STATE_SWITCH = "icon_comp_switch";
+
 	public static final String IMG_STATE_TRANSFER = "icon_comp_transfer";
 
 	@Override
-	protected void addAvailableImages() {
+	protected void addAvailableImages()
+	{
 
 		addImageFilePath(IMG_TRANSACTION_PAD, "icons/transition.gif"); //$NON-NLS-1$
 		addImageFilePath(IMG_RELATION_PAD, "icons/relation.gif"); //$NON-NLS-1$
@@ -123,6 +155,70 @@ public class CoreImageProvider extends AbstractImageProvider {
 		addImageFilePath("success", "icons/events/icon_event_success.png");
 		addImageFilePath("transferred", "icons/events/icon_event_transferred.png");
 		addImageFilePath("unknown", "icons/events/icon_event_unknown.png");
-	}
 
+		
+		
+//		addImageFilePath("account_on", "icon/events/icon_event_account_on.png");
+//		addImageFilePath("alta_on", "icon/events/icon_event_alta_on.png");
+//		addImageFilePath("baja_on", "icon/events/icon_event_baja_on.png");
+//		addImageFilePath("broker_on", "icon/events/icon_event_broker_on.png");
+//		addImageFilePath("busy_on", "icon/events/icon_event_busy_on.png");
+//		addImageFilePath("card_on", "icon/events/icon_event_card_on.png");
+//		addImageFilePath("conn_error_on", "icon/events/icon_event_conn_error_on.png");
+//		addImageFilePath("consulta_on", "icon/events/icon_event_consulta_on.png");
+//		addImageFilePath("editar_on", "icon/events/icon_event_editar_on.png");
+//		addImageFilePath("error_on", "icon/events/icon_event_error_on.png");
+//		addImageFilePath("far_end_disc_on", "icon/events/icon_event_far_end_disc_on.png");
+//		addImageFilePath("hangup_on", "icon/events/icon_event_hangup_on.png");
+//		addImageFilePath("loans_on", "icon/events/icon_event_loans_on.png");
+//		addImageFilePath("match_on", "icon/events/icon_event_match_on.png");
+//		addImageFilePath("maxatt_on", "icon/events/icon_event_maxatt_on.png");
+//		addImageFilePath("maxtime_on", "icon/events/icon_event_maxtime_on.png");
+//		addImageFilePath("mortgages_on", "icon/events/icon_event_mortgages_on.png");
+//		addImageFilePath("netw_busy_on", "icon/events/icon_event_netw_busy_on.png");
+//		addImageFilePath("netw_disconnect_on", "icon/events/icon_event_netw_disconnect_on.png");
+//		addImageFilePath("noanswer_on", "icon/events/icon_event_noanswer_on.png");
+//		addImageFilePath("noimput_on", "icon/events/icon_event_noimput_on.png");
+//		addImageFilePath("nomatch_on", "icon/events/icon_event_nomatch_on.png");
+//		addImageFilePath("noresource_on", "icon/events/icon_event_noresource_on.png");
+//		addImageFilePath("payments_on", "icon/events/icon_event_payments_on.png");
+//		addImageFilePath("pinManagement_on", "icon/events/icon_event_pinManagement_on.png");
+//		addImageFilePath("recorded_on", "icon/events/icon_event_recorded_on.png");
+//		addImageFilePath("record_unsup_on", "icon/events/icon_event_record_unsup_on.png");
+//		addImageFilePath("success_on", "icon/events/icon_event_success_on.png");
+//		addImageFilePath("transferred_on", "icon/events/icon_event_transferred_on.png");
+//		addImageFilePath("unknown_on", "icon/events/icon_event_unknown_on.png");
+//		
+//		addImageFilePath("account_off", "icon/events/icon_event_account_off.png");
+//		addImageFilePath("alta_off", "icon/events/icon_event_alta_off.png");
+//		addImageFilePath("baja_off", "icon/events/icon_event_baja_off.png");
+//		addImageFilePath("broker_off", "icon/events/icon_event_broker_off.png");
+//		addImageFilePath("busy_off", "icon/events/icon_event_busy_off.png");
+//		addImageFilePath("card_off", "icon/events/icon_event_card_off.png");
+//		addImageFilePath("conn_error_off", "icon/events/icon_event_conn_error_off.png");
+//		addImageFilePath("consulta_off", "icon/events/icon_event_consulta_off.png");
+//		addImageFilePath("editar_off", "icon/events/icon_event_editar_off.png");
+//		addImageFilePath("error_off", "icon/events/icon_event_error_off.png");
+//		addImageFilePath("far_end_disc_off", "icon/events/icon_event_far_end_disc_off.png");
+//		addImageFilePath("hangup_off", "icon/events/icon_event_hangup_off.png");
+//		addImageFilePath("loans_off", "icon/events/icon_event_loans_off.png");
+//		addImageFilePath("match_off", "icon/events/icon_event_match_off.png");
+//		addImageFilePath("maxatt_off", "icon/events/icon_event_maxatt_off.png");
+//		addImageFilePath("maxtime_off", "icon/events/icon_event_maxtime_off.png");
+//		addImageFilePath("mortgages_off", "icon/events/icon_event_mortgages_off.png");
+//		addImageFilePath("netw_busy_off", "icon/events/icon_event_netw_busy_off.png");
+//		addImageFilePath("netw_disconnect_off", "icon/events/icon_event_netw_disconnect_off.png");
+//		addImageFilePath("noanswer_off", "icon/events/icon_event_noanswer_off.png");
+//		addImageFilePath("noimput_off", "icon/events/icon_event_noimput_off.png");
+//		addImageFilePath("nomatch_off", "icon/events/icon_event_nomatch_off.png");
+//		addImageFilePath("noresource_off", "icon/events/icon_event_noresource_off.png");
+//		addImageFilePath("payments_off", "icon/events/icon_event_payments_off.png");
+//		addImageFilePath("pinManagement_off", "icon/events/icon_event_pinManagement_off.png");
+//		addImageFilePath("recorded_off", "icon/events/icon_event_recorded_off.png");
+//		addImageFilePath("record_unsup_off", "icon/events/icon_event_record_unsup_off.png");
+//		addImageFilePath("success_off", "icon/events/icon_event_success_off.png");
+//		addImageFilePath("transferred_off", "icon/events/icon_event_transferred_off.png");
+//		addImageFilePath("unknown_off", "icon/events/icon_event_unknown_off.png");
+		
+	}
 }
