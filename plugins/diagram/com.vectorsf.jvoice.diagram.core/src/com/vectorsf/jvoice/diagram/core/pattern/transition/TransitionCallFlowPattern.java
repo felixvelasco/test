38b79adcc6f07cbc3a21a6548a4fd877d3bdeef1
idafereyes <idafe.reyes@servexternos.isban.es>
2014-01-30 17:59:06 +0100

    Changed transition name from image id to event name.

--- plugins/diagram/com.vectorsf.jvoice.diagram.core/src/com/vectorsf/jvoice/diagram/core/pattern/transition/TransitionCallFlowPattern.java
+++ plugins/diagram/com.vectorsf.jvoice.diagram.core/src/com/vectorsf/jvoice/diagram/core/pattern/transition/TransitionCallFlowPattern.java
@@ -2,62 +2,64 @@ package com.vectorsf.jvoice.diagram.core.pattern.transition;
 
 import org.eclipse.graphiti.features.IFeatureProvider;
 import org.eclipse.graphiti.features.context.ICreateConnectionContext;
-import org.eclipse.graphiti.mm.algorithms.*;
-import org.eclipse.graphiti.mm.pictograms.*;
+import org.eclipse.graphiti.mm.algorithms.GraphicsAlgorithm;
+import org.eclipse.graphiti.mm.algorithms.Image;
+import org.eclipse.graphiti.mm.algorithms.Text;
+import org.eclipse.graphiti.mm.pictograms.FixPointAnchor;
+import org.eclipse.graphiti.mm.pictograms.PictogramElement;
+import org.eclipse.graphiti.services.Graphiti;
 
 import com.vectorsf.jvoice.model.operations.State;
 import com.vectorsf.jvoice.model.operations.Transition;
 
 public class TransitionCallFlowPattern extends TransitionPattern {
-    protected State state;
-    private IFeatureProvider featureProvider;
-    private String eventName;
+	protected State state;
+	private IFeatureProvider featureProvider;
+	private String eventName;
 
-    public TransitionCallFlowPattern(State stateFinal,
-	    IFeatureProvider in_featureProvider) {
-	super(in_featureProvider);
-	state = stateFinal;
-	featureProvider = in_featureProvider;
-	eventName = state.getName();
-    }
-
-    @Override
-    public String getCreateName() {
-	return eventName;
-	// return state.getName();
-    }
-
-    @Override
-    public boolean canCreate(ICreateConnectionContext context) {
-	if (state != null) {
-
-	    PictogramElement pe = context.getSourcePictogramElement();
-
-	    // Al crear la transición desde el evento lo que llega es un
-	    // FixPointAnchor
-	    if (pe instanceof FixPointAnchor) {
-		GraphicsAlgorithm ga = pe.getGraphicsAlgorithm();
-
-		if (ga instanceof Image) {
-		    eventName = ((Image) ga).getId();
-		} else {
-		    eventName = ((Text) ga).getValue();
-		}
-		pe = ((FixPointAnchor) pe).getParent();
-	    }
-
-	    Object boSource = featureProvider
-		    .getBusinessObjectForPictogramElement(pe);
-
-	    State stateOrigen = (State) boSource;
-
-	    for (Transition transition : stateOrigen.getOutgoingTransitions()) {
-		if (transition.getEventName().equals(state.getName())) {
-		    return false;
-		}
-	    }
+	public TransitionCallFlowPattern(State stateFinal, IFeatureProvider in_featureProvider) {
+		super(in_featureProvider);
+		state = stateFinal;
+		featureProvider = in_featureProvider;
+		eventName = state.getName();
 	}
 
-	return super.canCreate(context);
-    }
+	@Override
+	public String getCreateName() {
+		return eventName;
+		// return state.getName();
+	}
+
+	@Override
+	public boolean canCreate(ICreateConnectionContext context) {
+		if (state != null) {
+
+			PictogramElement pe = context.getSourcePictogramElement();
+
+			// Al crear la transición desde el evento lo que llega es un
+			// FixPointAnchor
+			if (pe instanceof FixPointAnchor) {
+				GraphicsAlgorithm ga = pe.getGraphicsAlgorithm();
+
+				if (ga instanceof Image) {
+					eventName = Graphiti.getPeService().getPropertyValue(pe, "TOOLTIP");
+				} else {
+					eventName = ((Text) ga).getValue();
+				}
+				pe = ((FixPointAnchor) pe).getParent();
+			}
+
+			Object boSource = featureProvider.getBusinessObjectForPictogramElement(pe);
+
+			State stateOrigen = (State) boSource;
+
+			for (Transition transition : stateOrigen.getOutgoingTransitions()) {
+				if (transition.getEventName().equals(state.getName())) {
+					return false;
+				}
+			}
+		}
+
+		return super.canCreate(context);
+	}
 }
