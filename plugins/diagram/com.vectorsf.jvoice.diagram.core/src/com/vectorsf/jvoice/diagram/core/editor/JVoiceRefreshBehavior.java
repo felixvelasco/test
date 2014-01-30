235dc562f5d22f9d448c1bcfc8f834af53598419
José Luis Gonzalo <jgonzalo99@gmail.com>
2014-01-30 17:42:12 +0100

    Refresca los estados al arrancar el editor

--- plugins/diagram/com.vectorsf.jvoice.diagram.core/src/com/vectorsf/jvoice/diagram/core/editor/JVoiceRefreshBehavior.java
+++ plugins/diagram/com.vectorsf.jvoice.diagram.core/src/com/vectorsf/jvoice/diagram/core/editor/JVoiceRefreshBehavior.java
@@ -1,21 +1,34 @@
 package com.vectorsf.jvoice.diagram.core.editor;
 
+import org.eclipse.graphiti.dt.IDiagramTypeProvider;
+import org.eclipse.graphiti.features.IFeatureProvider;
+import org.eclipse.graphiti.features.context.impl.UpdateContext;
+import org.eclipse.graphiti.mm.pictograms.Diagram;
+import org.eclipse.graphiti.mm.pictograms.Shape;
 import org.eclipse.graphiti.ui.editor.DefaultRefreshBehavior;
 import org.eclipse.graphiti.ui.editor.DiagramBehavior;
 
-/**
- * Para que refresque los estados al arrancar el editor. Por ejemplo, que actualice los options del estado Menu.
- */
 public class JVoiceRefreshBehavior extends DefaultRefreshBehavior {
 
 	public JVoiceRefreshBehavior(DiagramBehavior diagramBehavior) {
 		super(diagramBehavior);
 	}
 
+	/**
+	 * Refresca los estados al arrancar el editor.
+	 */
 	@Override
 	protected void autoUpdate() {
-		super.autoUpdate();
-		System.out.println("#########################JVoiceRefreshBehavior.autoUpdate(): ");
+		IDiagramTypeProvider diagramTypeProvider = diagramBehavior.getDiagramTypeProvider();
+
+		IFeatureProvider fp = diagramTypeProvider.getFeatureProvider();
+		Diagram diagram = diagramTypeProvider.getDiagram();
+
+		for (Shape shape : diagram.getChildren()) {
+			fp.updateIfPossible(new UpdateContext(shape));
+		}
+
+		refresh();
 	}
 
 }
