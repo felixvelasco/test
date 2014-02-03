74bbe180ebddbc845e0f914d1ffe00c3d1f9f8a1
José Luis Gonzalo <jgonzalo99@gmail.com>
2014-02-03 15:36:19 +0100

    Cambiado el color de la fuente del mensaje de error de las propiedades del estado switch

--- plugins/ui/com.vectorsf.jvoice.ui.diagram.properties/src/com/vectorsf/jvoice/ui/diagram/properties/editting/ValidateListener.java
+++ plugins/ui/com.vectorsf.jvoice.ui.diagram.properties/src/com/vectorsf/jvoice/ui/diagram/properties/editting/ValidateListener.java
@@ -6,31 +6,36 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CLabel;
 import org.eclipse.ui.PlatformUI;
 
-
 public class ValidateListener implements ICellEditorListener {
 
 	private TextCellEditor testCell;
 	private CLabel error;
-	
+
 	public ValidateListener(TextCellEditor testCell, CLabel error) {
-	    this.testCell = testCell;
-	    this.error = error;
-	  }
-	
-	@Override
-	public void applyEditorValue() {}
+		this.testCell = testCell;
+		this.error = error;
+	}
 
 	@Override
-	public void cancelEditor() {}
+	public void applyEditorValue() {
+	}
+
+	@Override
+	public void cancelEditor() {
+	}
 
 	@Override
 	public void editorValueChanged(boolean oldValidState, boolean newValidState) {
 		if (!newValidState) {
 			error.setText(testCell.getErrorMessage());
-			error.setBackground(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell().getDisplay().getSystemColor(SWT.COLOR_RED));
-		}else{
+			error.setBackground(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell().getDisplay()
+					.getSystemColor(SWT.COLOR_RED));
+			error.setForeground(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell().getDisplay()
+					.getSystemColor(SWT.COLOR_WHITE));
+		} else {
 			error.setText("");
-			error.setBackground(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell().getDisplay().getSystemColor(SWT.INHERIT_DEFAULT));
+			error.setBackground(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell().getDisplay()
+					.getSystemColor(SWT.INHERIT_DEFAULT));
 		}
 	}
 }
