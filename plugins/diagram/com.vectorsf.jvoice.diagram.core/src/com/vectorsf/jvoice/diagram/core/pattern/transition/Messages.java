c9acb818a437882827649a3f78511f0a22fd7b60
José Luis Gonzalo <jgonzalo99@gmail.com>
2014-02-03 15:34:20 +0100

    Quitados literales que no se usan

--- plugins/diagram/com.vectorsf.jvoice.diagram.core/src/com/vectorsf/jvoice/diagram/core/pattern/transition/Messages.java
+++ plugins/diagram/com.vectorsf.jvoice.diagram.core/src/com/vectorsf/jvoice/diagram/core/pattern/transition/Messages.java
@@ -6,10 +6,7 @@ public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "com.vectorsf.jvoice.diagram.core.pattern.transition.messages"; //$NON-NLS-1$
 	public static String AddMoreEventFeature_moreEvents;
 	public static String AddMoreEventFeature_dialogTitle;
-	public static String AddMoreEventFeature_name;
 	public static String AddMoreEventFeature_newEvent;
-	public static String AddMoreEventFeature_selectName;
-	public static String AddMoreEventFeature_addCustomEvent;
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
