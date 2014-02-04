84853aa19d86ccaec609f5696efe0f9d764be1a3
Esther Mateos <emateos@ext.vectorsf.com>
2014-02-04 15:58:23 +0100

    cambiado versiones de dsl builder y application a 1.0.1 y 1.0.2 en la creacion de proyectos

--- plugins/core/com.vectorsf.jvoice.core.project/src/com/vectorsf/jvoice/core/project/AbstractJVoiceNature.java
+++ plugins/core/com.vectorsf.jvoice.core.project/src/com/vectorsf/jvoice/core/project/AbstractJVoiceNature.java
@@ -21,9 +21,9 @@ public abstract class AbstractJVoiceNature implements IProjectNature {
 	public static final String PROJECT_INFORMATION_FILE = ".projectInformation"; //$NON-NLS-1$
 	public static final String PLUGIN_ID = "com.vectorsf.jvoice.core.project"; //$NON-NLS-1$
 	public static final String NATURE_ID = PLUGIN_ID + ".jVoiceApplicationNature"; //$NON-NLS-1$
-	public static final String JVOICE_FRAMEWORK_VERSION = "1.0.1";
-	public static final String JVOICE_COMPILER_VERSION = "1.0.0";
-	public static final String JVOICE_REPOSITORY_URL = "http://repository-felixvelasco.forge.cloudbees.com/release";
+	public static final String JVOICE_FRAMEWORK_VERSION = "1.0.2-SNAPSHOT";
+	public static final String JVOICE_COMPILER_VERSION = "1.0.1-SNAPSHOT";
+	public static final String JVOICE_REPOSITORY_URL = "http://repository-felixvelasco.forge.cloudbees.com/snapshot";
 
 	private IProject project;
 
