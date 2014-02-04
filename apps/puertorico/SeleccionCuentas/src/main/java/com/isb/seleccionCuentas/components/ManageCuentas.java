c103cdabf45962c3089d407c7c85c1abe8faef6f
Alberto Fernández <alberto.fernandezsanchez@servexternos.isban.es>
2014-02-04 17:36:38 +0100

    Cuentas PR App

--- apps/puertorico/SeleccionCuentas/src/main/java/com/isb/seleccionCuentas/components/ManageCuentas.java
+++ apps/puertorico/SeleccionCuentas/src/main/java/com/isb/seleccionCuentas/components/ManageCuentas.java
@@ -1,10 +1,13 @@
 package com.isb.seleccionCuentas.components;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import com.isb.global.components.Account;
 import com.isb.global.components.CallData;
 
 @Component
@@ -13,16 +16,37 @@ public class ManageCuentas
 {
 	@Inject
 	Text text;
+	@Inject
+	AuxClass aux;
 	CallData data;
 	
 	
 	public String manageTexts()
-	{
-		int nInd = Integer.parseInt(text.getIndice());
-		String numCuenta = data.getListaCuentas().get(nInd).getNumCuenta();
-		text.setTexto("Para la cuenta que termina en "+numCuenta.substring(numCuenta.length()-4)+", presione "+(nInd+1));
-		nInd++;
-		text.setIndice(""+nInd);
+	{		
+		StringBuffer sbText = new StringBuffer();
+		List<Account> lCuen = data.getListaCuentas();
+		if (aux.getVuelta().equals("1"))
+		{
+			for (int i = 0; i < lCuen.size()&& i < 5; i++) 
+			{
+				Account accounti = lCuen.get(i);
+				sbText.append("Para la cuenta que termina en "+accounti.getNumCuenta().substring(accounti.getNumCuenta().length()-4)+", presione "+(i+1)+". ");
+			}
+			if (lCuen.size()>5)
+			{
+				sbText.append("Para listar las siguientes cuentas presione 6.");
+			}
+		}
+		else
+		{
+			for (int i = 0; i < lCuen.size()&& i < 10; i++) 
+			{
+				Account accounti = lCuen.get(i);
+				sbText.append("Para la cuenta que termina en "+accounti.getNumCuenta().substring(accounti.getNumCuenta().length()-4)+", presione "+(i+1)+". ");
+			}			
+		}
+		aux.incVuelta();	
+		text.setTexto(sbText.toString());
 		return null;
 	}
 }
