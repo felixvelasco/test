c5a438b7d13ec0e7390816e9bf6c2daa923d3d2e
Alberto Fernández <alberto.fernandezsanchez@servexternos.isban.es>
2014-02-04 17:48:33 +0100

    paginación vuelta 2: cuentas

--- apps/puertorico/SeleccionCuentas/src/main/java/com/isb/seleccionCuentas/components/ManageCuentas.java
+++ apps/puertorico/SeleccionCuentas/src/main/java/com/isb/seleccionCuentas/components/ManageCuentas.java
@@ -25,6 +25,7 @@ public class ManageCuentas
 	{		
 		StringBuffer sbText = new StringBuffer();
 		List<Account> lCuen = data.getListaCuentas();
+		// se listan las 5 primeras
 		if (aux.getVuelta().equals("1"))
 		{
 			for (int i = 0; i < lCuen.size()&& i < 5; i++) 
@@ -37,9 +38,10 @@ public class ManageCuentas
 				sbText.append("Para listar las siguientes cuentas presione 6.");
 			}
 		}
+		// se listan las 5 segundas
 		else
 		{
-			for (int i = 0; i < lCuen.size()&& i < 10; i++) 
+			for (int i = 5; i < lCuen.size()&& i < 10; i++) 
 			{
 				Account accounti = lCuen.get(i);
 				sbText.append("Para la cuenta que termina en "+accounti.getNumCuenta().substring(accounti.getNumCuenta().length()-4)+", presione "+(i+1)+". ");
