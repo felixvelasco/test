4b979bfcf99f2a683be141875c0a35f7872e9bef
idafereyes <idafe.reyes@servexternos.isban.es>
2014-02-20 11:30:46 +0100

    Adding a component to diagram core tests resources.

---
+++ tests/com.vectorsf.jvoice.diagram.core.test/resources/components/TestExecute.java
@@ -0,0 +1,14 @@
+package com.isb.sWTTests.components;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+public class TestExecute {
+	
+	public void test(){
+		
+	}
+
+}
