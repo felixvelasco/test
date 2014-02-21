cdf43bf1f305480b295c849c1cee6c5556c5234b
idafereyes <idafe.reyes@servexternos.isban.es>
2014-02-21 14:30:17 +0100

    Corrected open states test from diagram core tests.

--- tests/com.vectorsf.jvoice.diagram.core.test/src/com/vectorsf/jvoice/diagram/core/test/IVRDiagramOpenStatesTest.java
+++ tests/com.vectorsf.jvoice.diagram.core.test/src/com/vectorsf/jvoice/diagram/core/test/IVRDiagramOpenStatesTest.java
@@ -6,9 +6,7 @@ import static com.vectorsf.jvoice.base.test.ResourcesHelper.deleteProject;
 import static com.vectorsf.jvoice.base.test.ResourcesHelper.getInputStreamResource;
 import static org.eclipse.swtbot.swt.finder.finders.UIThreadRunnable.syncExec;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.arrayContaining;
 import static org.hamcrest.Matchers.emptyArray;
-import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.fail;
 
@@ -19,6 +17,9 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.gef.GraphicalEditPart;
 import org.eclipse.graphiti.ui.editor.IDiagramContainerUI;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotEditor;
@@ -26,7 +27,6 @@ import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
 import org.eclipse.swtbot.eclipse.gef.finder.SWTGefBot;
 import org.eclipse.swtbot.eclipse.gef.finder.widgets.SWTBotGefEditPart;
 import org.eclipse.swtbot.eclipse.gef.finder.widgets.SWTBotGefEditor;
-import org.eclipse.swtbot.eclipse.gef.finder.widgets.SWTBotGefViewer;
 import org.eclipse.swtbot.swt.finder.finders.UIThreadRunnable;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.eclipse.swtbot.swt.finder.results.VoidResult;
@@ -35,8 +35,6 @@ import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.ide.IDE;
-import org.hamcrest.Matcher;
-import org.hamcrest.Matchers;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -46,7 +44,6 @@ import org.osgi.framework.Bundle;
 
 import com.vectorsf.jvoice.base.model.service.BaseModel;
 import com.vectorsf.jvoice.base.test.SWTBotHelper;
-import com.vectorsf.jvoice.model.operations.State;
 
 /**
  * 
@@ -54,19 +51,39 @@ import com.vectorsf.jvoice.model.operations.State;
 @RunWith(SWTBotJunit4ClassRunner.class)
 public class IVRDiagramOpenStatesTest {
 
-	private static final int SMALL_SLEEP = 200;
 	private static final int LARGE_SLEEP = 1000;
 	private static final String NAVIGATOR_ID = "com.vectorsf.jvoice.ui.navigator.ViewIVR";
 	protected static SWTGefBot bot = new SWTGefBot();
 	private SWTBotView view;
 	private SWTBotGefEditor editor;
-	private SWTBotGefViewer gefViewer;
-	@SuppressWarnings("unchecked")
-	private final Matcher<Object[]> arrayContainingTextOne = arrayContaining(hasProperty(
-			"text", is("one")));
-	@SuppressWarnings("unchecked")
-	private final Matcher<Object[]> arrayContainingTextTwo = arrayContaining(hasProperty(
-			"text", is("two")));
+	private IProject project;
+	private IFile flowFile;
+
+	private static final String PROJECT_NAME = "testNavigator";
+	private static final String PACKAGE_PATH = "/several/packages/inside/";
+	private static final String FLOW_NAME = "everyState.jvflow";
+	private static final String FLOW_RESOURCES_FOLDER = "everyState.resources/";
+	private static final String COMPONENTS_PATH = "/com/isb/testNavigator/components/";
+
+	// Nombres de los estados del flujo de prueba.
+	private static final String OUTPUT_STATE_NAME = "Output";
+	private static final String INPUT_STATE_NAME = "Input";
+	private static final String MENU_STATE_NAME = "Menu";
+	private static final String RECORD_STATE_NAME = "Record";
+	private static final String TRANSFER_STATE_NAME = "Transfer";
+	private static final String CUSTOM_STATE_NAME = "Custom";
+	private static final String EXECUTE_STATE_NAME = "test";
+	private static final String SUBFLOW_STATE_NAME = "empty";
+
+	// Nombres de los resources asociados a los estados del flujo de prueba.
+	private static final String OUTPUT_RES_NAME = "Output";
+	private static final String INPUT_RES_NAME = "Input";
+	private static final String MENU_RES_NAME = "Menu";
+	private static final String RECORD_RES_NAME = "Record";
+	private static final String TRANSFER_RES_NAME = "Transfer";
+	private static final String CUSTOM_RES_NAME = "Custom";
+	private static final String EXECUTE_RES_NAME = "TestExecute";
+	private static final String SUBFLOW_RES_NAME = "empty";
 
 	public static final Bundle bundle = Platform
 			.getBundle("com.vectorsf.jvoice.diagram.core.test");
@@ -95,6 +112,17 @@ public class IVRDiagramOpenStatesTest {
 		SWTBotHelper.openView(bot, "IVR", "Navigator IVR");
 
 		view = bot.viewById(NAVIGATOR_ID);
+
+		// Comprobamos que al empezar el test no haya ningún proyecto creado.
+		assertThat(view.bot().tree().getAllItems(), is(emptyArray()));
+
+		// Cremos el proyecto de prueba
+		project = createProject(PROJECT_NAME);
+
+		// Creamos el flujo de prueba
+		flowFile = createFile(project, BaseModel.JV_PATH + PACKAGE_PATH
+				+ FLOW_NAME,
+				getInputStreamResource(bundle, "flows/" + FLOW_NAME));
 
 	}
 
@@ -153,93 +181,214 @@ public class IVRDiagramOpenStatesTest {
 		});
 	}
 
-	private Matcher<Iterable<? super State>> hasStateNamed(String stateName) {
-		return Matchers.<State> hasItem(hasProperty("name", is(stateName)));
-	}
-
 	@Test
-	public void testOpenCallFlow() throws Exception {
-		openState("empty", 425, 65);
+	public void testOpenSubflow() throws Exception {
+
+		// Creamos el recurso que debe abrirse al abrir (doble-click) el estado.
+		createFile(
+				project,
+				BaseModel.JV_PATH + PACKAGE_PATH + SUBFLOW_RES_NAME + ".jvflow",
+				getInputStreamResource(bundle, "flows/" + SUBFLOW_RES_NAME
+						+ ".jvflow"));
+
+		// Abrimos el estado
+		openState(SUBFLOW_STATE_NAME);
+
+		// Comprobamos que se ha abierto el recurso asociado al estado
+		assertThat(isOpen(SUBFLOW_RES_NAME), is(true));
+
 	}
 
 	@Test
 	public void testOpenMenu() throws Exception {
-		openState("Menu", 305, 275);
+
+		// Creamos el recurso que debe abrirse al abrir (doble-click) el estado.
+		createFile(
+				project,
+				BaseModel.JV_PATH + PACKAGE_PATH + FLOW_RESOURCES_FOLDER
+						+ MENU_RES_NAME + ".voiceDsl",
+				getInputStreamResource(bundle, "voiceDsls/" + MENU_RES_NAME
+						+ ".voiceDsl"));
+		// Abrimos el estado
+		openState(MENU_STATE_NAME);
+
+		// Comprobamos que se ha abierto el recurso asociado al estado
+		assertThat(isOpen(MENU_RES_NAME), is(true));
+
 	}
 
 	@Test
 	public void testOpenInput() throws Exception {
-		openState("Input", 265, 175);
+
+		// Creamos el recurso que debe abrirse al abrir (doble-click) el estado.
+		createFile(
+				project,
+				BaseModel.JV_PATH + PACKAGE_PATH + FLOW_RESOURCES_FOLDER
+						+ INPUT_RES_NAME + ".voiceDsl",
+				getInputStreamResource(bundle, "voiceDsls/" + INPUT_RES_NAME
+						+ ".voiceDsl"));
+
+		// Abrimos el estado
+		openState(INPUT_STATE_NAME);
+
+		// Comprobamos que se ha abierto el recurso asociado al estado
+		assertThat(isOpen(INPUT_RES_NAME), is(true));
+
 	}
 
 	@Test
-	public void testOpenPrompt() throws Exception {
-		openState("Prompt", 90, 175);
+	public void testOpenOutput() throws Exception {
+
+		// Creamos el recurso que debe abrirse al abrir (doble-click) el estado.
+		createFile(
+				project,
+				BaseModel.JV_PATH + PACKAGE_PATH + FLOW_RESOURCES_FOLDER
+						+ OUTPUT_RES_NAME + ".voiceDsl",
+				getInputStreamResource(bundle, "voiceDsls/" + OUTPUT_RES_NAME
+						+ ".voiceDsl"));
+
+		// Abrimos el estado
+		openState(OUTPUT_STATE_NAME);
+
+		// Comprobamos que se ha abierto el recurso asociado al estado
+		assertThat(isOpen(OUTPUT_RES_NAME), is(true));
+
 	}
 
-	public void openState(final String stateName, final int x, final int y)
-			throws Exception {
-		assertThat(view.bot().tree().getAllItems(), is(emptyArray()));
+	@Test
+	public void testOpenRecord() throws Exception {
 
-		IProject project = createProject("testNavigator");
-		IFile file = createFile(project, BaseModel.JV_PATH
-				+ "/several/packages/inside/six.jvflow",
-				getInputStreamResource(bundle, "six.jvflow"));
-		createFile(project, BaseModel.JV_PATH
-				+ "/several/packages/inside/empty.jvflow",
-				getInputStreamResource(bundle, "empty.jvflow"));
-		createFile(project, BaseModel.JV_PATH
-				+ "/several/packages/inside/Menu.voiceDsl",
-				getInputStreamResource(bundle, "Menu.voiceDsl"));
-		createFile(project, BaseModel.JV_PATH
-				+ "/several/packages/inside/Input.voiceDsl",
-				getInputStreamResource(bundle, "Input.voiceDsl"));
-		createFile(project, BaseModel.JV_PATH
-				+ "/several/packages/inside/Prompt.voiceDsl",
-				getInputStreamResource(bundle, "Prompt.voiceDsl"));
-		openFile(file);
+		// Creamos el recurso que debería abrirse al abrir (doble-click) el
+		// estado.
+		createFile(
+				project,
+				BaseModel.JV_PATH + PACKAGE_PATH + FLOW_RESOURCES_FOLDER
+						+ RECORD_RES_NAME + ".voiceDsl",
+				getInputStreamResource(bundle, "voiceDsls/" + RECORD_RES_NAME
+						+ ".voiceDsl"));
+		// Abrimos el estado
+		openState(RECORD_STATE_NAME);
+
+		// Comprobamos que se ha abierto el recurso asociado al estado
+		assertThat(isOpen(RECORD_RES_NAME), is(true));
+
+	}
+
+	@Test
+	public void testOpenTransfer() throws Exception {
+
+		// Creamos el recurso que debería abrirse al abrir (doble-click) el
+		// estado.
+		createFile(
+				project,
+				BaseModel.JV_PATH + PACKAGE_PATH + FLOW_RESOURCES_FOLDER
+						+ TRANSFER_RES_NAME + ".voiceDsl",
+				getInputStreamResource(bundle, "voiceDsls/" + TRANSFER_RES_NAME
+						+ ".voiceDsl"));
+
+		// Abrimos el estado
+		openState(TRANSFER_STATE_NAME);
+
+		// Comprobamos que se ha abierto el recurso asociado al estado
+		assertThat(isOpen(TRANSFER_RES_NAME), is(true));
+
+	}
+
+	@Test
+	public void testOpenCustom() throws Exception {
+
+		// Creamos el recurso que debería abrirse al abrir (doble-click) el
+		// estado.
+		createFile(
+				project,
+				BaseModel.JV_PATH + PACKAGE_PATH + FLOW_RESOURCES_FOLDER
+						+ CUSTOM_RES_NAME + ".jsp",
+				getInputStreamResource(bundle, "jsps/" + CUSTOM_RES_NAME
+						+ ".jsp"));
+
+		// Abrimos el estado
+		openState(CUSTOM_STATE_NAME);
+
+		// Comprobamos que se ha abierto el recurso asociado al estado
+		assertThat(isOpen(CUSTOM_RES_NAME), is(true));
+
+	}
+
+	@Test
+	public void testOpenExecute() throws Exception {
+
+		// Creamos el recurso que debería abrirse al abrir (doble-click) el
+		// estado.
+		createFile(
+				project,
+				BaseModel.JAVA_SOURCES_PATH + COMPONENTS_PATH
+						+ EXECUTE_RES_NAME + ".java",
+				getInputStreamResource(bundle, "components/" + EXECUTE_RES_NAME
+						+ ".java"));
+
+		// Abrimos el estado
+		openState(EXECUTE_STATE_NAME);
+
+		// Comprobamos que se ha abierto el recurso asociado al estado
+		assertThat(isOpen(EXECUTE_RES_NAME), is(true));
+
+	}
+
+	public void openState(final String stateName) throws Exception {
+
+		openFile(flowFile);
+
 		bot.sleep(LARGE_SLEEP);
 
 		editor = getGefEditor();
-		gefViewer = editor.getSWTBotGefViewer();
-
-		SWTBotGefEditPart entity = editor.getEditPart(stateName);
 
 		syncExec(new VoidResult() {
-
 			@Override
 			public void run() {
 
+				// Recuperamos la figura asociada el estado que queremos abrir
+				// para poner el ratón sobre la figura y poder seleccionarlo y
+				// posteriormente hacer doble-click. Esto es necesario por la
+				// implementación de graphiti.
+				SWTBotGefEditPart entity = editor.getEditPart(stateName);
+				final IFigure figure = ((GraphicalEditPart) entity.part())
+						.getFigure();
+				final Rectangle bounds = figure.getBounds().getCopy();
+				figure.translateToAbsolute(bounds);
+
 				IDiagramContainerUI diagramEditor = (IDiagramContainerUI) editor
 						.getReference().getEditor(true);
-				diagramEditor.getDiagramBehavior().getMouseLocation().x = x;
-				diagramEditor.getDiagramBehavior().getMouseLocation().y = y;
+				diagramEditor.getDiagramBehavior().getMouseLocation().x = bounds.x;
+				diagramEditor.getDiagramBehavior().getMouseLocation().y = bounds.y;
+
+				// Seleccionamos el estado
 				editor.select(stateName);
 			}
 		});
+
 		bot.sleep(LARGE_SLEEP);
+
+		// Doble click sobre el estado para abrirlo
 		editor.doubleClick(stateName);
-		// if (stateName != "empty") {
-		// bot.waitUntil(shellIsActive("Add Xtext Nature"), 10000);
-		// if (shellIsActive("Add Xtext Nature") != null) {
-		// SWTBotShell shell = bot.shell("Add Xtext Nature");
-		// shell.bot().button("Yes").click();
-		// }
-		// }
 
-		editor.save();
+	}
 
+	private Boolean isOpen(String resourceName) {
+
+		// Comprobamos que esté abierta una ventana que contenga el nombre
+		// del recurso que debería haberse abierto.
 		SWTBotShell[] shells = bot.shells();
 		boolean hasShell = false;
 		for (int i = 0; i < shells.length; i++) {
 			if (shells[i].isOpen()) {
 				SWTBotShell shell = shells[i];
-				if (shell.getText().contains(stateName)) {
+				if (shell.getText().contains(resourceName)) {
 					hasShell = true;
+					break;
 				}
 			}
 		}
-		assertThat(hasShell, is(true));
+		return hasShell;
 	}
 
 	public SWTBotGefEditor getGefEditor() {
