5a66ce340a1960191d062cdcf1ecd97f29cfb7db
Esther Mateos <emateos@ext.vectorsf.com>
2014-03-04 15:08:23 +0100

    changed to version 1.2.0-SNAPSHOT

--- plugins/core/com.vectorsf.jvoice.core.project/src/com/vectorsf/jvoice/core/project/AbstractJVoiceNature.java
+++ plugins/core/com.vectorsf.jvoice.core.project/src/com/vectorsf/jvoice/core/project/AbstractJVoiceNature.java
@@ -21,9 +21,9 @@ public abstract class AbstractJVoiceNature implements IProjectNature {
 	public static final String PROJECT_INFORMATION_FILE = ".projectInformation"; //$NON-NLS-1$
 	public static final String PLUGIN_ID = "com.vectorsf.jvoice.core.project"; //$NON-NLS-1$
 	public static final String NATURE_ID = PLUGIN_ID + ".jVoiceApplicationNature"; //$NON-NLS-1$
-	public static final String JVOICE_FRAMEWORK_VERSION = "1.1.0-SNAPSHOT";
-	public static final String JVOICE_COMPILER_VERSION = "1.1.0-SNAPSHOT";
-	public static final String JVOICE_REPOSITORY_URL = "http://repository-felixvelasco.forge.cloudbees.com/snapshot";
+	public static final String JVOICE_FRAMEWORK_VERSION = "1.2.0-SNAPSHOT";
+	public static final String JVOICE_COMPILER_VERSION = "1.2.0-SNAPSHOT";
+	public static final String JVOICE_REPOSITORY_URL = "http://repository-felixvelasco.forge.cloudbees.com/snapshot/snapshot/";
 	public static final String JVOICE_REPOSITORY_NAME = "Jvoice snapshot repository";
 	public static final String JVOICE_REPOSITORY_ID = "jvoiceSnapshot";
 
