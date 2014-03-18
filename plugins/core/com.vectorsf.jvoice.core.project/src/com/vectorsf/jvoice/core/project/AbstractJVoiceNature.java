12d3912dedc4cae758e77be0359cd6cd89c613ef
Felix Velasco <fvelascov@ext.vectorsf.com>
2014-03-18 13:07:35 +0100

    Raise version to 1.2.1

--- plugins/core/com.vectorsf.jvoice.core.project/src/com/vectorsf/jvoice/core/project/AbstractJVoiceNature.java
+++ plugins/core/com.vectorsf.jvoice.core.project/src/com/vectorsf/jvoice/core/project/AbstractJVoiceNature.java
@@ -22,7 +22,7 @@ public abstract class AbstractJVoiceNature implements IProjectNature {
 	public static final String PLUGIN_ID = "com.vectorsf.jvoice.core.project"; //$NON-NLS-1$
 	public static final String NATURE_ID = PLUGIN_ID + ".jVoiceApplicationNature"; //$NON-NLS-1$
 	public static final String JVOICE_FRAMEWORK_VERSION = "1.2.0";
-	public static final String JVOICE_COMPILER_VERSION = "1.2.0";
+	public static final String JVOICE_COMPILER_VERSION = "1.2.1";
 
 //	public static final String JVOICE_REPOSITORY_URL = "http://repository-felixvelasco.forge.cloudbees.com/snapshot/snapshot/";
 //	public static final String JVOICE_REPOSITORY_NAME = "Jvoice snapshot repository";
