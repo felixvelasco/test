04f932811e948787cfc576b3cbd48d48db9c0653
Felix Velasco <fvelascov@ext.vectorsf.com>
2014-03-20 17:09:47 +0100

    Bug #8295: Save only the diagram resource.

--- plugins/diagram/com.vectorsf.jvoice.diagram.core/src/com/vectorsf/jvoice/diagram/core/editor/JVoicePersistencyBehavior.java
+++ plugins/diagram/com.vectorsf.jvoice.diagram.core/src/com/vectorsf/jvoice/diagram/core/editor/JVoicePersistencyBehavior.java
@@ -13,7 +13,6 @@ import org.eclipse.core.resources.IWorkspaceRunnable;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.common.util.WrappedException;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -57,34 +56,20 @@ public class JVoicePersistencyBehavior extends DefaultPersistencyBehavior {
 									.getActiveTransaction().getParent()) != null);
 						}
 
-						final EList<Resource> resources = editingDomain.getResourceSet().getResources();
-						// Copy list to an array to prevent
-						// ConcurrentModificationExceptions
-						// during the saving of the dirty resources
-						Resource[] resourcesArray = new Resource[resources.size()];
-						resourcesArray = resources.toArray(resourcesArray);
-						for (int i = 0; i < resourcesArray.length; i++) {
-							// In case resource modification tracking is
-							// switched on,
-							// we can check if a resource has been modified, so
-							// that we only need to save
-							// really changed resources; otherwise we need to
-							// save all resources in the set
-							final Resource resource = resourcesArray[i];
-							/*
-							 * Bug 371513 - Added check for isLoaded(): a resource that has not yet been loaded
-							 * (possibly after a reload triggered by a change in another editor) has no content yet;
-							 * saving such a resource will simply erase _all_ content from the resource on the disk
-							 * (including the diagram). --> a not yet loaded resource must not be saved
-							 */
-							if ((!resource.isTrackingModification() || resource.isModified()) && resource.isLoaded()
-									&& resource instanceof XMLResource) {
-								try {
-									resource.save(saveOptions.get(resource));
-									savedResources.add(resource);
-								} catch (final RuntimeException | IOException t) {
-									failedSaves.put(resource.getURI(), t);
-								}
+						Resource mainResource = editingDomain.getResourceSet().getResources().get(0);
+						/*
+						 * Bug 371513 - Added check for isLoaded(): a resource that has not yet been loaded (possibly
+						 * after a reload triggered by a change in another editor) has no content yet; saving such a
+						 * resource will simply erase _all_ content from the resource on the disk (including the
+						 * diagram). --> a not yet loaded resource must not be saved
+						 */
+						if ((!mainResource.isTrackingModification() || mainResource.isModified())
+								&& mainResource.isLoaded() && mainResource instanceof XMLResource) {
+							try {
+								mainResource.save(saveOptions.get(mainResource));
+								savedResources.add(mainResource);
+							} catch (final RuntimeException | IOException t) {
+								failedSaves.put(mainResource.getURI(), t);
 							}
 						}
 					}
