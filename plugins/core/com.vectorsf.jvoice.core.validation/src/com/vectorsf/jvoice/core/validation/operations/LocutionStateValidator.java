b77375903e7a2bd80492ca0cd4a1c8f73ac1358c
Esther Mateos <emateos@ext.vectorsf.com>
2014-04-02 15:11:44 +0200

    Validations of programmatic states

--- plugins/core/com.vectorsf.jvoice.core.validation/src/com/vectorsf/jvoice/core/validation/operations/LocutionStateValidator.java
+++ plugins/core/com.vectorsf.jvoice.core.validation/src/com/vectorsf/jvoice/core/validation/operations/LocutionStateValidator.java
@@ -2,7 +2,17 @@ package com.vectorsf.jvoice.core.validation.operations;
 
 import java.io.File;
 
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaModelException;
+
+import com.vectorsf.jvoice.core.operation.helper.FlowService;
+import com.vectorsf.jvoice.model.operations.Flow;
+import com.vectorsf.jvoice.model.operations.InputState;
 import com.vectorsf.jvoice.model.operations.LocutionState;
+import com.vectorsf.jvoice.model.operations.PromptState;
+import com.vectorsf.jvoice.model.operations.RecordState;
+import com.vectorsf.jvoice.model.operations.TransferState;
 
 public class LocutionStateValidator {
 
@@ -43,6 +53,65 @@ public class LocutionStateValidator {
 		}
 
 		return true;
+	}
+
+	// Comprobación para los estados programáticos del metodo y su salida.
+	public boolean validate_LocutionState_checkMethodLocution(LocutionState lstate) {
+
+		if (!lstate.isTextual()) {
+
+			Flow flow = (Flow) lstate.eContainer();
+
+			boolean exist = false;
+			boolean returnOk = false;
+			try {
+				IType helperType = FlowService.getHelperType(flow);
+				for (IMethod method : helperType.getMethods()) {
+					String declaredReturnType = method.getReturnType()
+							.substring(1, method.getReturnType().length() - 1);
+					String nameMethod = method.getElementName();
+					// Vamos a comprobar que el resultado metodo existe y que el tipo devuelto es el correcto.
+					if (nameMethod.equals(lstate.getMethodName())) {
+						exist = true;
+						if (declaredReturnType.equals(getTypeLocution(lstate))) {
+							returnOk = true;
+							break;
+
+						}
+					}
+				}
+			} catch (JavaModelException e1) {
+				e1.printStackTrace();
+			}
+
+			if (!exist) {
+				operationsValidator.error(lstate,
+						"Missing method '" + lstate.getMethodName() + "' in state '" + lstate.getName() + "'.");
+			}
+
+			if (!returnOk) {
+				operationsValidator.error(lstate, "The return object of method '" + lstate.getMethodName()
+						+ "' in state '" + lstate.getName() + "' is not correct.");
+			}
+
+		} else {
+			operationsValidator.error(lstate, "The state '" + lstate.getName() + "' has no associated Method.'");
+		}
+
+		return true;
+	}
+
+	private String getTypeLocution(LocutionState lstate) {
+		if (lstate instanceof InputState) {
+			return "Input";
+		} else if (lstate instanceof PromptState) {
+			return "Output";
+		} else if (lstate instanceof TransferState) {
+			return "Transfer";
+		} else if (lstate instanceof RecordState) {
+			return "Record";
+		}
+		return null;
 	}
 
 }
--- plugins/core/com.vectorsf.jvoice.core.validation/src/com/vectorsf/jvoice/core/validation/operations/MenuStateValidator.java
+++ plugins/core/com.vectorsf.jvoice.core.validation/src/com/vectorsf/jvoice/core/validation/operations/MenuStateValidator.java
@@ -3,12 +3,19 @@ package com.vectorsf.jvoice.core.validation.operations;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaModelException;
+
+import com.vectorsf.jvoice.core.operation.helper.FlowService;
+import com.vectorsf.jvoice.model.operations.Flow;
 import com.vectorsf.jvoice.model.operations.MenuState;
 import com.vectorsf.jvoice.model.operations.Transition;
 import com.vectorsf.jvoice.prompt.model.voiceDsl.Output;
 
 public class MenuStateValidator {
 	private OperationsValidator operationsValidator;
+	private final String PATH = "src/main/java";
 
 	public MenuStateValidator(OperationsValidator operationsValidator) {
 		this.operationsValidator = operationsValidator;
@@ -20,13 +27,65 @@ public class MenuStateValidator {
 			transitions.add(t.getEventName());
 		}
 
-		for (Output o : state.getLocution().getOutputs().getOutput()) {
-			if (!transitions.contains(o.getName())) {
-				operationsValidator.error(state, "Missing transition for option '" + o.getName() + "' in state '"
-						+ state.getName() + "'.");
+		if (state.isTextual()) {
+			if (state.getLocution() != null) {
+				for (Output o : state.getLocution().getOutputs().getOutput()) {
+					if (!transitions.contains(o.getName())) {
+						operationsValidator.error(state, "Missing transition for option '" + o.getName()
+								+ "' in state '" + state.getName() + "'.");
+					}
+				}
+			} else {
+				operationsValidator.error(state, "Definition State " + state.getName() + " has not a dsl associated.");
 			}
 		}
 
 		return true;
 	}
+
+	public boolean validate_MenuState_checkMenuMethod(MenuState state) {
+
+		if (!state.isTextual()) {
+
+			Flow flow = (Flow) state.eContainer();
+
+			boolean exist = false;
+			boolean returnOk = false;
+			try {
+				IType helperType = FlowService.getHelperType(flow);
+				for (IMethod method : helperType.getMethods()) {
+					String declaredReturnType = method.getReturnType()
+							.substring(1, method.getReturnType().length() - 1);
+					String nameMethod = method.getElementName();
+					// Vamos a comprobar que el resultado metodo existe y que el tipo devuelto es el correcto.
+					if (nameMethod.equals(state.getMethodName())) {
+						exist = true;
+						if (declaredReturnType.equals("Menu")) {
+							returnOk = true;
+							break;
+
+						}
+					}
+				}
+			} catch (JavaModelException e1) {
+				e1.printStackTrace();
+			}
+
+			if (!exist) {
+				operationsValidator.error(state,
+						"Missing method '" + state.getMethodName() + "' in state '" + state.getName() + "'.");
+			}
+
+			if (!returnOk) {
+				operationsValidator.error(state, "The return object of of method '" + state.getMethodName()
+						+ "' in state '" + state.getName() + "' is not correct.");
+			}
+
+		} else {
+			operationsValidator.error(state, "The state '" + state.getName() + "' has no associated Method.'");
+		}
+
+		return true;
+	}
+
 }
